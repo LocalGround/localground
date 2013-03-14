2dadd2c24b22994b50ddf9d492f764304e233f11
vanwars <vanwars@gmail.com>
2013-03-14 09:13:57 -0700

    Added ability to pull in MapBox tilesets

--- static/scripts/helpers/mapTypes.js
+++ static/scripts/helpers/mapTypes.js
@@ -43,7 +43,7 @@ CloudMadeType.prototype.getTile = function(coord, zoom, ownerDocument) {
 CloudMadeType.prototype.name = this.name;
 
 
-//Cloudmade:
+//Stamen:
 function StamenType(opts) {
     this.styleID = 1
     this.maxZoom = opts.max;
@@ -64,6 +64,30 @@ StamenType.prototype.getTile = function(coord, zoom, ownerDocument) {
     }).get(0);
 };
 StamenType.prototype.name = this.name;
+
+
+//MapBox:
+function MapBoxType(opts) {
+    this.maxZoom = opts.max;
+    this.styleID = opts.styleID
+    this.name = opts.name
+};
+
+// static map url:
+// http://api.tiles.mapbox.com/v3/nps.pt-shaded-relief,nps.pt-urban-areas,nps.pt-river-lines,nps.pt-admin-lines,nps.pt-park-poly,nps.pt-mask,nps.pt-hydro-features,nps.pt-admin-labels,nps.pt-roads,nps.pt-road-shields,nps.pt-park-points,nps.pt-hydro-labels,nps.pt-city-labels,nps.pt-park-labels/pin-m-monument%28-77.04,38.89%29/-77.04,38.89,13/640x640.png
+MapBoxType.prototype.tileSize = new google.maps.Size(256,256);
+MapBoxType.prototype.getTile = function(coord, zoom, ownerDocument) {
+    //cycle between a, b, and c sub-domains:
+    var url = 'http://' + ['a.', 'b.', 'c.', 'd.'][parseInt(Math.random()*4)] + 'tiles.mapbox.com/v3/';
+    return $('<div></div>').css({
+        'width': '256px',
+        'height': '256px',
+        'backgroundImage': 'url(' + url + this.styleID + '/' + zoom + '/' +
+                                coord.x + '/' + coord.y + '.png)'
+
+    }).get(0);
+};
+MapBoxType.prototype.name = this.name;
 
 
 
