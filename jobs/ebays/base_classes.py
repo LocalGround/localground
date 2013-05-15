511f80f200cde31cf4cfd22d3b024aaa099572ed
vanwars <vanwars@gmail.com>
2013-05-15 07:29:10 -0700

    added some debugging to the EBAYS particulate matter data loader

--- jobs/ebays/base_classes.py
+++ jobs/ebays/base_classes.py
@@ -44,10 +44,12 @@ class DataFile(object):
     '''
     def print_records(self, limit=None):
         if limit is None:
-            for rec in self.records:
+            for i, rec in enumerate(self.records):
                 print rec
+                if i % 500 == 0:
+                    raw_input('waiting...')
         else:
-            for rec in self.records[:limit]:
+            for i, rec in enumerate(self.records[:limit]):
                 print rec
             
             
@@ -60,7 +62,7 @@ class DataFile(object):
             
 class SpatialFile(DataFile):
     
-    def zip_coordinates(self, coordinate_records):
+    def zip_coordinates_old(self, coordinate_records):
         print 'Number of particle records: %s' % len(self.records)
         print 'Number of GPS records: %s' % len(coordinate_records)
         geocoded = 0
@@ -81,6 +83,9 @@ class SpatialFile(DataFile):
                             geocoded += 1
                             self.records[i].lat = coordinate_records[j].lat
                             self.records[i].lng = coordinate_records[j].lng
+                            
+                        print i, j, len(coordinate_records), data_rec.format_datetime(), coordinate_records[j].format_datetime(), \
+                            data_rec.format_datetime() == coordinate_records[j].format_datetime()
                     except:
                         pass
                     
@@ -91,8 +96,41 @@ class SpatialFile(DataFile):
                 print 'Number of GPS records: %s' % len(coordinate_records)
                 print 'Record counter (i): %s' % i
                 print 'GPS record counter (j): %s' % j
-                raw_input('Press Enter to print records')
-                self.print_records()
+                #raw_input('Press Enter to print records')
+                #self.print_records()
         print '%s out of %s geocoded' % (geocoded, len(self.records))
         #self.print_records()
+        
+        
+    def zip_coordinates(self, coordinate_records):
+        num_data_recs = len(self.records)
+        num_gps_recs = len(coordinate_records)
+        print 'Number of particle records: %s' % num_data_recs
+        print 'Number of GPS records: %s' % num_gps_recs
+        geocoded, i, j = 0, 0, 0
+        while i < num_data_recs - 1 and j < num_gps_recs - 1:
+            data_rec = self.records[i]
+            gps_rec = coordinate_records[j]
+            print i, j, data_rec.date_time, gps_rec.date_time, data_rec.date_time == gps_rec.date_time
+            if data_rec.date_time is None:
+                i += 1
+            elif gps_rec.date_time is None:
+                j += 1
+            elif data_rec.date_time < gps_rec.date_time:
+                i += 1
+            elif data_rec.date_time > gps_rec.date_time:
+                j += 1
+            else:
+                self.records[i].lat = gps_rec.lat
+                self.records[i].lng = gps_rec.lng
+                geocoded += 1
+                i += 1
+                j += 1
+            if j % 500 == 0:
+                raw_input('waiting...')
+        print '%s out of %s geocoded' % (geocoded, num_data_recs)
+        raw_input('waiting...')
+        #471 2193        
+                  
+                
 
