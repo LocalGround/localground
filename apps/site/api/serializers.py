3bafbd8e21d246bdbedc3a53c03cff7b6e0d2d70
Sarah <vanwars@gmail.com>
2013-08-06 17:08:36 -0700

    Added a Create Print test.

--- apps/site/api/serializers.py
+++ apps/site/api/serializers.py
@@ -308,17 +308,21 @@ class OverlaySourceSerializer(serializers.ModelSerializer):
 
 
 class PrintSerializer(ExtentsSerializer):
+	layout_url = serializers.HyperlinkedRelatedField(view_name='layout-detail', source='layout', read_only=True)
+	layout = serializers.SlugRelatedField(label='layout', slug_field='id')
+	map_provider = serializers.SlugRelatedField(label='map_provider', slug_field='id')
+	map_provider_url = serializers.HyperlinkedRelatedField(view_name='wmsoverlay-detail', source='map_provider', read_only=True)
 	pdf = serializers.SerializerMethodField('get_pdf')
 	thumb = serializers.SerializerMethodField('get_thumb')
 	uuid = serializers.SerializerMethodField('get_uuid')
-	instructions = serializers.WritableField(label='instructions', source='description')
-	map_title = serializers.WritableField(label='map_title', source='name')
+	instructions = serializers.WritableField(label='instructions', source='description', blank=True)
+	map_title = serializers.WritableField(label='map_title', source='name', blank=True)
 	
 	class Meta:
 		model = models.Print
 		fields = ('id', 'uuid', 'project_id', 'map_title', 'instructions',
-					'tags', 'overlay_type', 'pdf', 'thumb', 'zoom',
-					'map_provider', 'layout', 'center')
+					'tags', 'overlay_type', 'pdf', 'thumb', 'zoom', 'map_provider',
+					'map_provider_url', 'layout', 'layout_url', 'center')
 		depth = 0
 		
 	def get_pdf(self, obj):
--- apps/site/api/tests/prints.py
+++ apps/site/api/tests/prints.py
@@ -3,6 +3,7 @@ from localground.apps.site import models
 from localground.apps.site.api import views
 from localground.apps.site.tests import ViewMixin
 import urllib
+from rest_framework import status
 
 class ApiLayoutListTest(test.TestCase, ViewMixin):
 	def setUp(self):
@@ -22,31 +23,42 @@ class ApiPrintListTest(test.TestCase, ViewMixin):
 		self.url = '/api/0/prints/'
 		self.urls =  [self.url]
 		self.view = views.PrintList.as_view()
-		
-	'''	
+			
 	def test_create_print_using_post(self, **kwargs):
-		lat, lng, name, description, color = 54.16, 60.4, 'New Marker 1', \
-							'Test description1', 'FF0000'
+		lat, lng = 54.16, 60.4
+		map_title = 'A Map Title'
+		instructions = 'Some nstructions.'
+		layout = 1
+		map_provider = 1
+		zoom = 17
+		
 		response = self.client.post(self.url,
 			data=urllib.urlencode({
 				'lat': lat,
 				'lng': lng,
-				'name': name,
-				'description': description,
-				'color': color,
+				'map_title': map_title,
+				'instructions': instructions,
+				'layout': layout,
+				'map_provider': map_provider,
+				'zoom': zoom,
 				'project_id': self.project.id
 			}),
 			HTTP_X_CSRFTOKEN=self.csrf_token,
 			content_type = "application/x-www-form-urlencoded"
 		)
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-		new_object = models.Print.objects.all().order_by('-id',)[0]
-		self.assertEqual(new_object.name, name)
-		self.assertEqual(new_object.description, description)
+		new_object = (models.Print.objects
+						.select_related('layout', 'map_provider')
+						.all()
+						.order_by('-id',))[0]
+		self.assertEqual(new_object.name, map_title)
+		self.assertEqual(new_object.description, instructions)
 		self.assertEqual(new_object.center.y, lat)
 		self.assertEqual(new_object.center.x, lng)
 		self.assertEqual(new_object.project.id, self.project.id)
-	'''
+		self.assertEqual(new_object.layout.id, layout)
+		self.assertEqual(new_object.map_provider.id, layout)
+
 
 '''
 class ApiPrintInstanceTest(test.TestCase, ViewMixin):
