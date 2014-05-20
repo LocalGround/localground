797b08440899b4c29a9708321d99976e10b347dd
Zachary MacHardy <zmmachar@cs.berkeley.edu>
2014-05-20 16:20:01 -0400

    Merged view creation and updating.  Updated some view handling behavior

--- apps/site/api/serializers/view_serializer.py
+++ apps/site/api/serializers/view_serializer.py
@@ -2,118 +2,123 @@ from localground.apps.site.api.serializers.base_serializer import BaseSerializer
 from localground.apps.site.api.serializers.photo_serializer import PhotoSerializer
 from localground.apps.site.api.serializers.barcoded_serializer import ScanSerializer
 from localground.apps.site.api.serializers.audio_serializer import AudioSerializer
-from localground.apps.site.api.serializers.form_serializer import create_record_serializer, create_compact_record_serializer
+from localground.apps.site.api.serializers.form_serializer import create_record_serializer, \
+    create_compact_record_serializer
 from localground.apps.site.api.serializers.marker_serializer import MarkerSerializerCounts
 from rest_framework import serializers
 from localground.apps.site import models
 from django.forms.widgets import Textarea
 from localground.apps.site.api import fields
 
+
 class ViewSerializer(BaseSerializer):
-	access = serializers.SerializerMethodField('get_access')
-	entities = fields.EntitiesField(widget=Textarea, required=False)
-	class Meta:
-		model = models.View
-		fields = BaseSerializer.Meta.fields + ('owner', 'slug', 'access', 'entities')
-		depth = 0
-		
-	def get_access(self, obj):
-		return obj.access_authority.name
-	
-		
+    access = serializers.SerializerMethodField('get_access')
+    entities = fields.EntitiesField(widget=Textarea, required=False)
+
+    class Meta:
+        model = models.View
+        fields = BaseSerializer.Meta.fields + ('owner', 'slug', 'access', 'entities')
+        depth = 0
+
+    def get_access(self, obj):
+        return obj.access_authority.name
+
+
 class ViewDetailSerializer(BaseSerializer):
-	children = serializers.SerializerMethodField('get_children')
-	entities = fields.EntitiesField(widget=Textarea, required=False)
-	class Meta:
-		model = models.View
-		fields = BaseSerializer.Meta.fields + (
-			'slug', 'children', 'entities'
-		)
-		depth = 0
+    children = serializers.SerializerMethodField('get_children')
+    entities = fields.EntitiesField(widget=Textarea, required=False)
 
-	def get_children(self, obj):
-		from django.contrib.contenttypes.models import ContentType
-		from localground.apps.site import models
-		candidates = [
-			models.Photo, models.Audio, models.Scan, models.Project, models.Marker
-		]
-		forms = (models.Form.objects
-					.select_related('projects')
-					.prefetch_related('field_set', 'field_set__data_type')
-					.filter(projects=obj)
-				)
-		for form in forms:
-			candidates.append(form.TableModel)
-		#this caches the ContentTypes so that we don't keep executing one-off queries
-		ContentType.objects.get_for_models(*candidates, concrete_model=False)
-		children = {
-			'photos': self.get_photos(obj),
-			'audio': self.get_audio(obj),
-			'scans': self.get_scans(obj),
-			'markers': self.get_markers(obj, forms)
-		}
+    class Meta:
+        model = models.View
+        fields = BaseSerializer.Meta.fields + (
+        'slug', 'children', 'entities'
+        )
+        depth = 0
 
-		#add table data:
-		for form in forms:
-			form_data = self.get_table_records(obj, form)
-			if len(form_data.get('data')) > 0:
-				children['form_%s' % form.id] = form_data
-		return children
+    def get_children(self, obj):
+        from django.contrib.contenttypes.models import ContentType
+        from localground.apps.site import models
 
-	def get_table_records(self, obj, form):
-		SerializerClass = create_compact_record_serializer(form)
-		data = SerializerClass(
-				form.TableModel.objects.get_objects(obj.owner, project=obj)
-				#form.get_data_query_lite(obj.owner, project=obj, limit=1000)
-			).data
-		d = self.serialize_list(
-				form.TableModel,
-				data,
-				name=form.name,
-				overlay_type='record',
-				model_name_plural='form_%s' % form.id
-			)
-		d.update({
-			'headers': [f.col_alias for f in form.fields]
-		})
-		return d
+        candidates = [
+            models.Photo, models.Audio, models.Scan, models.Project, models.Marker
+        ]
+        forms = (models.Form.objects
+                     .select_related('projects')
+                     .prefetch_related('field_set', 'field_set__data_type')
+                     .filter(projects=obj)
+        )
+        for form in forms:
+            candidates.append(form.TableModel)
+        #this caches the ContentTypes so that we don't keep executing one-off queries
+        ContentType.objects.get_for_models(*candidates, concrete_model=False)
+        children = {
+        'photos': self.get_photos(obj),
+        'audio': self.get_audio(obj),
+        'scans': self.get_scans(obj),
+        'markers': self.get_markers(obj, forms)
+        }
 
-	def get_photos(self, obj):
-		data = PhotoSerializer(
-				obj.photos, many=True, context={'request': {}}
-			).data
-		return self.serialize_list(models.Photo, data)
-	
-	def get_audio(self, obj):
-		data = AudioSerializer(
-				obj.audio, many=True, context={'request': {}}
-			).data
-		return self.serialize_list(models.Audio, data)
+        #add table data:
+        for form in forms:
+            form_data = self.get_table_records(obj, form)
+            if len(form_data.get('data')) > 0:
+                children['form_%s' % form.id] = form_data
+        return children
 
-	def get_scans(self, obj):
-		data = ScanSerializer(
-				obj.map_images, many=True, context={'request': {}}
-			).data
-		return self.serialize_list(models.Scan, data)
+    def get_table_records(self, obj, form):
+        SerializerClass = create_compact_record_serializer(form)
+        data = SerializerClass(
+            form.TableModel.objects.get_objects(obj.owner, project=obj)
+            #form.get_data_query_lite(obj.owner, project=obj, limit=1000)
+        ).data
+        d = self.serialize_list(
+            form.TableModel,
+            data,
+            name=form.name,
+            overlay_type='record',
+            model_name_plural='form_%s' % form.id
+        )
+        d.update({
+        'headers': [f.col_alias for f in form.fields]
+        })
+        return d
 
-	def get_markers(self, obj, forms):
-		data = MarkerSerializerCounts(
-				obj.markers, many=True, context={'request': {}}
-			).data
-		return self.serialize_list(models.Marker, data)
+    def get_photos(self, obj):
+        data = PhotoSerializer(
+            obj.photos, many=True, context={'request': {}}
+        ).data
+        return self.serialize_list(models.Photo, data)
+
+    def get_audio(self, obj):
+        data = AudioSerializer(
+            obj.audio, many=True, context={'request': {}}
+        ).data
+        return self.serialize_list(models.Audio, data)
+
+    def get_scans(self, obj):
+        data = ScanSerializer(
+            obj.map_images, many=True, context={'request': {}}
+        ).data
+        return self.serialize_list(models.Scan, data)
+
+    def get_markers(self, obj, forms):
+        data = MarkerSerializerCounts(
+            obj.markers, many=True, context={'request': {}}
+        ).data
+        return self.serialize_list(models.Marker, data)
 
 
-	def serialize_list(self, cls, data, name=None, overlay_type=None,
-					   model_name_plural=None):
-		if name is None:
-			name = cls.model_name_plural.title()
-		if overlay_type is None:
-			overlay_type = cls.model_name
-		if model_name_plural is None:
-			model_name_plural = cls.model_name_plural
-		return {
-			'id': model_name_plural,
-			'name': name,
-			'overlay_type': overlay_type,
-			'data': data
-		}
+    def serialize_list(self, cls, data, name=None, overlay_type=None,
+                       model_name_plural=None):
+        if name is None:
+            name = cls.model_name_plural.title()
+        if overlay_type is None:
+            overlay_type = cls.model_name
+        if model_name_plural is None:
+            model_name_plural = cls.model_name_plural
+        return {
+        'id': model_name_plural,
+        'name': name,
+        'overlay_type': overlay_type,
+        'data': data
+        }
--- apps/site/api/views/view_views.py
+++ apps/site/api/views/view_views.py
@@ -1,105 +1,107 @@
 from rest_framework import generics, status
 from localground.apps.site.api import serializers, filters
 from localground.apps.site.api.views.abstract_views import \
-	AuditCreate, AuditUpdate, QueryableListCreateAPIView
+    AuditCreate, AuditUpdate, QueryableListCreateAPIView
 from localground.apps.site import models
 from localground.apps.site.api.permissions import CheckProjectPermissions
 from django.db.models import Q
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 
+
 class ViewMixin(object):
-	
-	def post_save(self, obj, created=False):
-		'''
-		Saves all children:
-		'''
-		from localground.apps.lib.helpers import get_timestamp_no_milliseconds
-		import json
-		from django.db import connection, IntegrityError, DatabaseError
-		entities_str = self.request.DATA.get('entities')
-		if entities_str:
-			entities = json.loads(entities_str)
-			source_type = self.model.get_content_type()
-			source_id = obj.id
-			#1) clear out existing child media:
-			obj.entities.all().delete()
-			
-			#2) attach new child media:
-			for child in entities:
-				overlay_type = child.get('overlay_type')
-				entity_type = models.Base.get_model(
-								model_name=overlay_type
-							).get_content_type()
-				entity_ids = child.get('ids')
-				for entity_id in entity_ids:
-					a = models.GenericAssociation(
-						source_id=source_id,
-						source_type=source_type,
-						entity_id=entity_id,
-						entity_type=entity_type,
-						owner=self.request.user,
-						last_updated_by=self.request.user,
-						time_stamp=get_timestamp_no_milliseconds()
-					)
-					try:
-						a.save()
-					except IntegrityError, e:
-						self.warnings.append('duplicates removed')
-						connection._rollback()
-				
+    def post_save(self, obj, created=False):
+        '''
+        Saves all children:
+        '''
+        from localground.apps.lib.helpers import get_timestamp_no_milliseconds
+        import json
+        from django.db import connection, IntegrityError, DatabaseError
+
+        entities_str = self.request.DATA.get('entities')
+        if entities_str:
+            entities = json.loads(entities_str)
+            source_type = self.model.get_content_type()
+            source_id = obj.id
+            #1) clear out existing child media:
+            obj.entities.all().delete()
+
+            #2) attach new child media:
+            for child in entities:
+                overlay_type = child.get('overlay_type')
+                entity_type = models.Base.get_model(
+                    model_name=overlay_type
+                ).get_content_type()
+                entity_ids = child.get('ids')
+                for entity_id in entity_ids:
+                    a = models.GenericAssociation(
+                        source_id=source_id,
+                        source_type=source_type,
+                        entity_id=entity_id,
+                        entity_type=entity_type,
+                        owner=self.request.user,
+                        last_updated_by=self.request.user,
+                        time_stamp=get_timestamp_no_milliseconds()
+                    )
+                    try:
+                        a.save()
+                    except IntegrityError, e:
+                        self.warnings.append('duplicates removed')
+                        connection._rollback()
+
+
 class ViewList(QueryableListCreateAPIView, ViewMixin, AuditCreate):
-	error_messages = {}
-	warnings = []
-	serializer_class = serializers.ViewSerializer
-	filter_backends = (filters.SQLFilterBackend,)
-	model = models.View
-	paginate_by = 100
-	
-	def get_queryset(self):
-		if self.request.user.is_authenticated():
-			return self.model.objects.get_objects(self.request.user)
-		else:
-			return self.model.objects.get_objects_public(
-				access_key=self.request.GET.get('access_key')
-			)
-	
-	def pre_save(self, obj):
-		AuditCreate.pre_save(self, obj)
-		obj.access_authority = models.ObjectAuthority.objects.get(id=1)
-		
-	def post_save(self, obj, created=False):
-		ViewMixin.post_save(self, obj, created=False)
-		
-	def create(self, request, *args, **kwargs):
-		response = super(ViewList, self).create(request, *args, **kwargs)
-		if len(self.warnings) > 0:
-			response.data.update({'warnings': self.warnings})
-		if self.error_messages:
-			response.data = self.error_messages
-			response.status = status.HTTP_400_BAD_REQUEST
-		return response
-		
-	
+    error_messages = {}
+    warnings = []
+    serializer_class = serializers.ViewSerializer
+    filter_backends = (filters.SQLFilterBackend,)
+    model = models.View
+    paginate_by = 100
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated():
+            return self.model.objects.get_objects(self.request.user)
+        else:
+            return self.model.objects.get_objects_public(
+                access_key=self.request.GET.get('access_key')
+            )
+
+    def pre_save(self, obj):
+        AuditCreate.pre_save(self, obj)
+        obj.access_authority = models.ObjectAuthority.objects.get(id=1)
+
+    def post_save(self, obj, created=False):
+        ViewMixin.post_save(self, obj, created=False)
+
+    def create(self, request, *args, **kwargs):
+        response = super(ViewList, self).create(request, *args, **kwargs)
+        if len(self.warnings) > 0:
+            response.data.update({'warnings': self.warnings})
+        if self.error_messages:
+            response.data = self.error_messages
+            response.status = status.HTTP_400_BAD_REQUEST
+        return response
+
+
 class ViewInstance(generics.RetrieveUpdateDestroyAPIView, ViewMixin, AuditUpdate):
-	error_messages = {}
-	warnings = []
-	queryset = models.View.objects.select_related('owner').all()
-	serializer_class = serializers.ViewDetailSerializer
-	model = models.View
-	
-	def pre_save(self, obj):
-		AuditUpdate.pre_save(self, obj)
-		
-	def post_save(self, obj, created=False):
-		ViewMixin.post_save(self, obj, created=False)
-		
-	def update(self, request, *args, **kwargs):
-		response = super(ViewInstance, self).update(request, *args, **kwargs)
-		if len(self.warnings) > 0:
-			response.data.update({'warnings': self.warnings})
-		if self.error_messages:
-			response.data = self.error_messages
-			response.status = status.HTTP_400_BAD_REQUEST
-		return response
+    error_messages = {}
+    warnings = []
+    queryset = models.View.objects.select_related('owner').all()
+    serializer_class = serializers.ViewDetailSerializer
+    model = models.View
+
+    def pre_save(self, obj):
+        AuditUpdate.pre_save(self, obj)
+
+    def post_save(self, obj, created=False):
+        ViewMixin.post_save(self, obj, created=False)
+
+    def update(self, request, *args, **kwargs):
+        response = super(ViewInstance, self).update(request, *args, **kwargs)
+        if len(self.warnings) > 0:
+            response.data.update({'warnings': self.warnings})
+        if self.error_messages:
+            response.data = self.error_messages
+            response.status = status.HTTP_400_BAD_REQUEST
+        return response
 	
