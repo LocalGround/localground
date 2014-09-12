ae4e8db696db5f32e76cdfd8f0121af8670fbc01
Sarah Van Wart <vanwars@Sarahs-MacBook-Pro-2.local>
2014-09-12 12:35:43 -0700

    Mapped the new editor to /maps/edit/new

--- static/backbone/js/api/views/maps/mapEditor.js
+++ static/backbone/js/api/views/maps/mapEditor.js
@@ -1,118 +1,31 @@
-define(
-	[
-		"views/maps/basemap",
-		"views/maps/sidepanel/dataPanel",
-		"lib/maps/data/dataManager",
-		"views/maps/overlays/overlayManager",
-		"views/maps/overlays/bubbleManager"
-		
-	], function() {
-	/**
-	 * The top-level view class that harnesses all of the map editor
-	 * functionality. Also coordinates event triggers across all of
-	 * the constituent views.
-	 * @class MapEditor
-	 */
-	localground.maps.views.MapEditor = Backbone.View.extend({
-		/**
-		 * @lends localground.maps.views.MapEditor#
-		 */
-		
-		el: "body",
-		/** A {@link localground.maps.views.Basemap} object */
-		basemap: null,
-		/** A {@link localground.maps.data.DataManager} object */
-		dataManager: null,
-		
-		/** A {@link localground.maps.controls.OverlayManager} object */
-		overlayManager: null,
-		
-		/** A object that coordinates events across objects */
-		eventManager: _.extend({}, Backbone.Events),
-		
-		/** A {@link localground.maps.views.DataPanel} object */
-		dataPanel: null,
-		/** A Map InfoBubble */
-		infoBubble: null,
-		/** A Map InfoBubble tooltip */
-		toolTip: null,
-		/**
-		 * Initializes the BasemapView and all of the panels.
-		 * @method initialize
-		 * @param {Object} opts
-		 * Dictionary of initialization options
-		 */
-		initialize: function(opts) {
-			$.extend(this, opts);
+define([
+	"jquery",
+	"jquery.bootstrap",
+	"core",
+	"sandbox",
+	"views/maps/basemap",
+	"lib/maps/data/dataManager",
+	"views/maps/sidepanel/dataPanel",
+	"views/maps/overlays/overlayManager",
+	"views/maps/overlays/bubbleManager",
+	"lib/maps/controls/deleteMenu",
+	"lib/maps/controls/drawingManager"
+   ],
+	function($, bootstrap, CORE, Sandbox) {
+	
+		localground.maps.views.MapEditor = function(opts){
+			CORE.create_module("basemap", localground.maps.views.Basemap, opts);
+			CORE.create_module("data-manager", localground.maps.data.DataManager);
+			CORE.create_module("data-panel", localground.maps.views.DataPanel);
+			CORE.create_module("overlay-manager", localground.maps.views.OverlayManager);
+			CORE.create_module("bubble-manager", localground.maps.views.BubbleManager);
 			
-			/*this.panelManager = new PanelManagerView([
-				'data',
-				'symbols',
-				'presentations',
-				'downloads'
-			]);
-			*/
+			// really, these are submodules of the map; not sure how this should
+			// be organized. But nice that they're stand-alone:
+			CORE.create_module("delete-menu", localground.maps.controls.DeleteMenu);
+			CORE.create_module("map-drawing-toolbar", localground.maps.controls.DrawingManager);
 			
-			var sb = {
-				eventManager:this.eventManager,
-				getWorkspace: function(){
-					return JSON.parse(localStorage["workspace"]);
-				},
-				subscribe: function () { 
-					//not sure if error handling should go here...
-					if (core.is_obj(evts)) { 
-						core.registerEvents(evts, module_selector); 
-					}             
-				},  
-			};
-			
-			//listen for projects being added or removed:
-			this.attachEvents();
-			
-			this.basemap = new localground.maps.views.Basemap(sb, {
-				mapContainerID: "map_canvas",
-				defaultLocation: opts.defaultLocation,
-				includeSearchControl: true,
-				includeGeolocationControl: false,
-				activeMapTypeID: opts.activeMapTypeID,
-				overlays: opts.overlays
-			});
-			
-			this.bubbleManager = new localground.maps.views.InfoBubble(sb, {
-				map: this.basemap.map
-			});
-			
-			this.dataManager = new localground.maps.data.DataManager({
-				eventManager: this.eventManager
-			});
-			
-			/** Controls the map overlays */
-			this.overlayManager = new localground.maps.views.OverlayManager({
-				dataManager: this.dataManager,
-				eventManager: this.eventManager,
-				map: this.basemap.map
-			});
-			
-			/** Controls the side panel overlay listings */
-			this.dataPanel = new localground.maps.views.DataPanel({
-				dataManager: this.dataManager,
-				eventManager: this.eventManager,
-				basemap: this.basemap
-			});
-			this.$el.find('#panels').append(this.dataPanel.render().el);
-		},
-
-		/**
-		 * Adds event listeners to listen for window resize
-		 */
-		attachEvents: function(){
-			var that = this;
-			$(window).off('resize');
-			$(window).on('resize', function(){
-				that.dataPanel.resize();	
-			});
-			
-		}
+			CORE.start_all();
+		};
+		return localground.maps.views.MapEditor;
 	});
-	return localground.maps.views.MapEditor;
-});
