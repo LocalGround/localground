0c383e47c571dd7691c48fc290a1a74182dd0491
Zachary MacHardy <zmmachar@berkeley.edu>
2014-09-12 14:02:33 -0700

    miscellaneous formatting fixes

--- static/backbone/js/api/views/maps/basemap.js
+++ static/backbone/js/api/views/maps/basemap.js
@@ -1,170 +1,175 @@
 define(["backbone",
-		"lib/maps/controls/searchBox",
-		"lib/maps/controls/geolocation",
-		"lib/maps/controls/tileController"
-		],
-	function(Backbone) {
-	/**
-	 * A class that handles the basic Google Maps functionality,
-	 * including tiles, search, and setting the default location.
-	 * @class Basemap
-	 */
-	localground.maps.views.Basemap = Backbone.View.extend({
-		/**
-		 * @lends localground.maps.views.Basemap#
-		 */
-		
-		/** The google.maps.Map object */
-		map: null,
-		/** The default map type, if one is not specified */
-		activeMapTypeID: 1,
-		/** A boolean flag, indicating whether or not to
-		 *  include a search control */
-		tileManager: null,
-		/** A data structure containing user location preferences */
-		userProfile: null,
-		/** A data structure containing a default location */
-		defaultLocation: null,
-		/** An HTML Tag id where the map gets initialized */
-		mapContainerID: null,
-		
-		/**
-		 * Initializes the google map and it's corresponding controls,
-		 * based on an "opts" configuration object. Valid options described
-		 * below:
-		 * @method
-		 * @param {String} opts.mapContainerID
-		 * @param {Object} opts.defaultLocation
-		 * @param {Boolean} opts.includeSearchControl
-		 * @param {Boolean} opts.includeGeolocationControl
-		 * @param {Integer} opts.activeMapTypeID
-		 * The user's preferred tileset for the given map.
-		 * @param {Array} opts.overlays
-		 * A list of available tilesets, based on user's profile.
-		 */
-		destroy: function(){
-			alert("bye");	
-		},
-		initialize: function(sb, opts){
-			this.sb = sb;
-			$.extend(this, opts);
-			this.restoreState();
-			
-			//render map:
-			this.renderMap();
-			
-			//add a search control, if requested:
-			if (opts.includeSearchControl)
-				this.searchControl = new localground.maps.controls.SearchBox(this.map);
-			
-			//add a browser-based location detector, if requested:
-			if (opts.includeGeolocationControl) {
-				this.geolocationControl = new localground.maps.controls.GeoLocation({
-					map: this.map,
-					userProfile: this.userProfile,
-					defaultLocation: this.defaultLocation
-				});
-			}
-			
-			//set up the various map tiles in Google maps:
-			if(this.overlays) {
-				this.tileManager = new localground.maps.controls.TileController(sb, {
-					map: this.map,
-					overlays: this.overlays,
-					activeMapTypeID: this.activeMapTypeID
-				})
-			}
-			//add event handlers:
-			this.addEventHandlers(sb);
-		},
-		
-		/**
-		 * Helper method that initializes the Google map (before
-		 * the corresponding controls are added).
-		 */
-		renderMap: function(){
-			var mapOptions = {
-				scrollwheel: false,
-				minZoom: this.minZoom,
-				streetViewControl: true,
-				scaleControl: true,
-				panControl: false,
-				mapTypeControlOptions: {
-					style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-					position: google.maps.ControlPosition.TOP_LEFT
-				},
-				zoomControlOptions: {
-					style: google.maps.ZoomControlStyle.SMALL
-				},
-				styles: [{
-					featureType: "poi.school",
-					elementType: "geometry", 
-					stylers: [ { saturation: -79 }, { lightness: 75 } ]
-				}],
-				zoom: this.defaultLocation.zoom,
-				center: this.defaultLocation.center
-				
-			};
-			this.map = new google.maps.Map(document.getElementById(this.mapContainerID),
-										mapOptions);
-			this.sb.setMap(this.map);
+    "lib/maps/controls/searchBox",
+    "lib/maps/controls/geolocation",
+    "lib/maps/controls/tileController"
+],
+    function (Backbone) {
+        /**
+         * A class that handles the basic Google Maps functionality,
+         * including tiles, search, and setting the default location.
+         * @class Basemap
+         */
+        localground.maps.views.Basemap = Backbone.View.extend({
+            /**
+             * @lends localground.maps.views.Basemap#
+             */
 
-            this.overlayView = new google.maps.OverlayView();
-            this.overlayView.draw = function() {};
-            this.overlayView.setMap(this.map);
-            this.sb.setOverlayView(this.overlayView)
+            /** The google.maps.Map object */
+            map: null,
+            /** The default map type, if one is not specified */
+            activeMapTypeID: 1,
+            /** A boolean flag, indicating whether or not to
+             *  include a search control */
+            tileManager: null,
+            /** A data structure containing user location preferences */
+            userProfile: null,
+            /** A data structure containing a default location */
+            defaultLocation: null,
+            /** An HTML Tag id where the map gets initialized */
+            mapContainerID: null,
 
+            /**
+             * Initializes the google map and it's corresponding controls,
+             * based on an "opts" configuration object. Valid options described
+             * below:
+             * @method
+             * @param {String} opts.mapContainerID
+             * @param {Object} opts.defaultLocation
+             * @param {Boolean} opts.includeSearchControl
+             * @param {Boolean} opts.includeGeolocationControl
+             * @param {Integer} opts.activeMapTypeID
+             * The user's preferred tileset for the given map.
+             * @param {Array} opts.overlays
+             * A list of available tilesets, based on user's profile.
+             */
+            destroy: function () {
+                alert("bye");
+            },
+            initialize: function (sb, opts) {
+                this.sb = sb;
+                $.extend(this, opts);
+                this.restoreState();
 
+                //render map:
+                this.renderMap();
 
-		},
-		addEventHandlers: function(sb){
-			//add notifications:
-			google.maps.event.addListener(this.map, "maptypeid_changed", function( evnt ) {
-				sb.notify({ type : "map-tiles-changed" });
-			});
-			google.maps.event.addListener(this.map, "idle", function( evnt ) {
-				sb.notify({ type : "map-extents-changed" });
-			});
+                //add a search control, if requested:
+                if (opts.includeSearchControl)
+                    this.searchControl = new localground.maps.controls.SearchBox(this.map);
 
-			//add listeners:
-			sb.listen({ 
-                "map-tiles-changed": this.saveState,
-				"map-extents-changed": this.saveState,
-                "item-drop"          : this.handleItemDrop
-			});
-			
-			//todo: possibly move to a layout module?
-			$(window).off('resize');
-			$(window).on('resize', function(){
-				sb.notify({ type : "window-resized" });
-			});
-		},
+                //add a browser-based location detector, if requested:
+                if (opts.includeGeolocationControl) {
+                    this.geolocationControl = new localground.maps.controls.GeoLocation({
+                        map: this.map,
+                        userProfile: this.userProfile,
+                        defaultLocation: this.defaultLocation
+                    });
+                }
 
-		saveState: function(data){
-			var latLng = this.map.getCenter();
-			this.sb.saveState({
-				center: [latLng.lng(), latLng.lat()],
-				zoom: this.map.getZoom(),
-				basemapID: this.tileManager.getMapTypeId()
-			});
-		},
-		restoreState: function(){
-			var state = this.sb.restoreState();
-			if (state) {
-				if (state.center) {
-					this.defaultLocation.center = new google.maps.LatLng(
-						state.center[1],
-						state.center[0]
-					);	
-				}
-				if (state.zoom)
-					this.defaultLocation.zoom = state.zoom;
-				if (state.basemapID)
-					this.activeMapTypeID = state.basemapID;
-			}
-		},
+                //set up the various map tiles in Google maps:
+                if (this.overlays) {
+                    this.tileManager = new localground.maps.controls.TileController(sb, {
+                        map: this.map,
+                        overlays: this.overlays,
+                        activeMapTypeID: this.activeMapTypeID
+                    })
+                }
+                //add event handlers:
+                this.addEventHandlers(sb);
+            },
+
+            /**
+             * Helper method that initializes the Google map (before
+             * the corresponding controls are added).
+             */
+            renderMap: function () {
+                var mapOptions = {
+                    scrollwheel: false,
+                    minZoom: this.minZoom,
+                    streetViewControl: true,
+                    scaleControl: true,
+                    panControl: false,
+                    mapTypeControlOptions: {
+                        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                        position: google.maps.ControlPosition.TOP_LEFT
+                    },
+                    zoomControlOptions: {
+                        style: google.maps.ZoomControlStyle.SMALL
+                    },
+                    styles: [
+                        {
+                            featureType: "poi.school",
+                            elementType: "geometry",
+                            stylers: [
+                                { saturation: -79 },
+                                { lightness: 75 }
+                            ]
+                        }
+                    ],
+                    zoom: this.defaultLocation.zoom,
+                    center: this.defaultLocation.center
+
+                };
+                this.map = new google.maps.Map(document.getElementById(this.mapContainerID),
+                    mapOptions);
+                this.sb.setMap(this.map);
+
+                this.overlayView = new google.maps.OverlayView();
+                this.overlayView.draw = function () {
+                };
+                this.overlayView.setMap(this.map);
+                this.sb.setOverlayView(this.overlayView)
 
 
-	});
-	return localground.maps.views.Basemap;
-});
+            },
+            addEventHandlers: function (sb) {
+                //add notifications:
+                google.maps.event.addListener(this.map, "maptypeid_changed", function (evnt) {
+                    sb.notify({ type: "map-tiles-changed" });
+                });
+                google.maps.event.addListener(this.map, "idle", function (evnt) {
+                    sb.notify({ type: "map-extents-changed" });
+                });
+
+                //add listeners:
+                sb.listen({
+                    "map-tiles-changed": this.saveState,
+                    "map-extents-changed": this.saveState,
+                    "item-drop": this.handleItemDrop
+                });
+
+                //todo: possibly move to a layout module?
+                $(window).off('resize');
+                $(window).on('resize', function () {
+                    sb.notify({ type: "window-resized" });
+                });
+            },
+
+            saveState: function (data) {
+                var latLng = this.map.getCenter();
+                this.sb.saveState({
+                    center: [latLng.lng(), latLng.lat()],
+                    zoom: this.map.getZoom(),
+                    basemapID: this.tileManager.getMapTypeId()
+                });
+            },
+            restoreState: function () {
+                var state = this.sb.restoreState();
+                if (state) {
+                    if (state.center) {
+                        this.defaultLocation.center = new google.maps.LatLng(
+                            state.center[1],
+                            state.center[0]
+                        );
+                    }
+                    if (state.zoom)
+                        this.defaultLocation.zoom = state.zoom;
+                    if (state.basemapID)
+                        this.activeMapTypeID = state.basemapID;
+                }
+            },
+
+
+        });
+        return localground.maps.views.Basemap;
+    });
--- static/backbone/js/api/views/maps/overlays/overlayGroup.js
+++ static/backbone/js/api/views/maps/overlays/overlayGroup.js
@@ -1,151 +1,157 @@
 define(
-	[
-		'backbone',
-		'config'
-	], function() {
-	/**
-	 * The top-level view class that harnesses all of the map editor
-	 * functionality. Also coordinates event triggers across all of
-	 * the constituent views.
-	 * @class OverlayGroup
-	 */
-	localground.maps.views.OverlayGroup = Backbone.View.extend({
-		/**
-		 * @lends localground.maps.views.OverlayGroup#
-		 */
-		
-		/** A google.maps.Map object */
-		map: null,
-		
-		/** A dictionary indexing the various
-		 * {@link localground.maps.overlays.Overlay} objects.
-		 */
-		overlays: {},
-		
-		visibleItems: null, 
-		
-		/** A Backbone.Collection object */
-		collection: null,
-		/**
-		 * Flag indicating whether or not the child elements should
-		 * be visible
-		 */
-		isVisible: false,
-		/** String that matches the collection's key */
-		key: null,
-		
-		/**
-		 * Initializes the OverlayGroup with a dictionary of options,
-		 * and adds event listeners that pertain to OverlayGroup
-		 * operations (batch operations like turn on everything, or
-		 * zoom to extents).
-		 */
-		initialize: function(sb, opts) {
-			this.sb = sb;
-			this.map = sb.getMap();
-			$.extend(this, opts);
-			this.overlays = {};
-			this.visibleItems = {}
-			this.key = this.collection.key;
-			var that = this;
+    [
+        'backbone',
+        'config'
+    ], function () {
+        /**
+         * The top-level view class that harnesses all of the map editor
+         * functionality. Also coordinates event triggers across all of
+         * the constituent views.
+         * @class OverlayGroup
+         */
+        localground.maps.views.OverlayGroup = Backbone.View.extend({
+            /**
+             * @lends localground.maps.views.OverlayGroup#
+             */
 
-			//listen for new data:
-			//this.collection.on('add', this.render, this);
-			this.listenTo(this.collection, 'add', this.render);
-            this.listenTo(this.collection, 'change', this.updateOverlay);
-			this.listenTo(this.collection, 'zoom-to-extent', this.zoomToExtent);
-			this.listenTo(this.collection, 'show-all', this.showAll);
-			this.listenTo(this.collection, 'hide-all', this.hideAll);
-			this.listenTo(this.collection, 'remove', this.removeItem);
-			
-			
-			//listen for map zoom change, re-render photo icons:
-			google.maps.event.addListener(this.map, 'zoom_changed', function() {
-				if(that.key != 'photos') { return; }
-				for(key in that.overlays){
-					var overlay = that.overlays[key];
-					overlay.getGoogleOverlay().setIcon(overlay.getIcon());
-				}	
-			});
-		},
-		
-		/**
-		 * A little tricky. Basically, this method is called every time
-		 * a new model is added to its corresponding collection in the
-		 * dataManager. The method creates a new overlay for each model
-		 * where the GeoJSON geometry is defined.
-		 * @param {Backbone.Model} model
-		 */
-		render: function(model) {
-			if (model.get("geometry") == null) { return; }
-			var key = model.collection.key;
-			var id = model.id;
-			//retrieve the corresponding overlay type from the config.js.
-			var configKey = key.split("_")[0];
-			this.overlays[id] = this.sb.loadSubmodule(
-				"overlay-" + model.getKey() + "-" + model.id,
-				localground.config.Config[configKey].Overlay,
-				{ model: model }
-			);
-		},
+            /** A google.maps.Map object */
+            map: null,
 
-        updateOverlay: function(model) {
-            if (model.get("geometry") == null) { return; }
-            var overlay = this.getOverlay(model);
-            if(overlay) overlay.remove();
-			var key = model.collection.key;
-			var id = model.id;
-			//retrieve the corresponding overlay type from the config.js.
-			var configKey = key.split("_")[0];
-			this.overlays[id] = this.sb.loadSubmodule(
-				"overlay-" + model.getKey() + "-" + model.id,
-				localground.config.Config[configKey].Overlay,
-				{ model: model }
-			);
-            this.overlays[id].changeMode();
-            this.overlays[id].show();
-        },
-		
-		/** Shows all of the map overlays */
-		showAll: function() {
-			this.isVisible = true;
-			for (key in  this.overlays)
-				this.overlays[key].show();
-		},
-		
-		/** Hides all of the map overlays */
-		hideAll: function() {
-			this.isVisible = false;
-			for (key in this.overlays)
-				this.overlays[key].hide();
-		},
-		
-		/** Zooms to the extent of the collection */
-		zoomToExtent: function() {
-			var bounds = new google.maps.LatLngBounds();
-			for (key in this.overlays) {
-				try {
-					//for polylines, polygons, and groundoverlays:
-					bounds.union(this.overlays[key].overlay.getBounds());
-				}
-				catch(e){
-					//for points:
-					bounds.extend(this.overlays[key].getCenter());	
-				}
-			}
-			this.map.fitBounds(bounds);
-		},
-		
-		/** Gets the overlay from the overlay dictionary */
-		getOverlay: function(model) {
-			return this.overlays[model.id];
-		},
-		destroy: function(){
-			this.remove();
-		},
-		removeItem: function(model){
-			delete this.overlays[model.id]
-		}
-	});
-	return localground.maps.views.OverlayGroup;
-});
+            /** A dictionary indexing the various
+             * {@link localground.maps.overlays.Overlay} objects.
+             */
+            overlays: {},
+
+            visibleItems: null,
+
+            /** A Backbone.Collection object */
+            collection: null,
+            /**
+             * Flag indicating whether or not the child elements should
+             * be visible
+             */
+            isVisible: false,
+            /** String that matches the collection's key */
+            key: null,
+
+            /**
+             * Initializes the OverlayGroup with a dictionary of options,
+             * and adds event listeners that pertain to OverlayGroup
+             * operations (batch operations like turn on everything, or
+             * zoom to extents).
+             */
+            initialize: function (sb, opts) {
+                this.sb = sb;
+                this.map = sb.getMap();
+                $.extend(this, opts);
+                this.overlays = {};
+                this.visibleItems = {}
+                this.key = this.collection.key;
+                var that = this;
+
+                //listen for new data:
+                //this.collection.on('add', this.render, this);
+                this.listenTo(this.collection, 'add', this.render);
+                this.listenTo(this.collection, 'change', this.updateOverlay);
+                this.listenTo(this.collection, 'zoom-to-extent', this.zoomToExtent);
+                this.listenTo(this.collection, 'show-all', this.showAll);
+                this.listenTo(this.collection, 'hide-all', this.hideAll);
+                this.listenTo(this.collection, 'remove', this.removeItem);
+
+
+                //listen for map zoom change, re-render photo icons:
+                google.maps.event.addListener(this.map, 'zoom_changed', function () {
+                    if (that.key != 'photos') {
+                        return;
+                    }
+                    for (key in that.overlays) {
+                        var overlay = that.overlays[key];
+                        overlay.getGoogleOverlay().setIcon(overlay.getIcon());
+                    }
+                });
+            },
+
+            /**
+             * A little tricky. Basically, this method is called every time
+             * a new model is added to its corresponding collection in the
+             * dataManager. The method creates a new overlay for each model
+             * where the GeoJSON geometry is defined.
+             * @param {Backbone.Model} model
+             */
+            render: function (model) {
+                if (model.get("geometry") == null) {
+                    return;
+                }
+                var key = model.collection.key;
+                var id = model.id;
+                //retrieve the corresponding overlay type from the config.js.
+                var configKey = key.split("_")[0];
+                this.overlays[id] = this.sb.loadSubmodule(
+                    "overlay-" + model.getKey() + "-" + model.id,
+                    localground.config.Config[configKey].Overlay,
+                    { model: model }
+                );
+            },
+
+            updateOverlay: function (model) {
+                if (model.get("geometry") == null) {
+                    return;
+                }
+                var overlay = this.getOverlay(model);
+                if (overlay) overlay.remove();
+                var key = model.collection.key;
+                var id = model.id;
+                //retrieve the corresponding overlay type from the config.js.
+                var configKey = key.split("_")[0];
+                this.overlays[id] = this.sb.loadSubmodule(
+                    "overlay-" + model.getKey() + "-" + model.id,
+                    localground.config.Config[configKey].Overlay,
+                    { model: model }
+                );
+                this.overlays[id].changeMode();
+                this.overlays[id].show();
+            },
+
+            /** Shows all of the map overlays */
+            showAll: function () {
+                this.isVisible = true;
+                for (key in  this.overlays)
+                    this.overlays[key].show();
+            },
+
+            /** Hides all of the map overlays */
+            hideAll: function () {
+                this.isVisible = false;
+                for (key in this.overlays)
+                    this.overlays[key].hide();
+            },
+
+            /** Zooms to the extent of the collection */
+            zoomToExtent: function () {
+                var bounds = new google.maps.LatLngBounds();
+                for (key in this.overlays) {
+                    try {
+                        //for polylines, polygons, and groundoverlays:
+                        bounds.union(this.overlays[key].overlay.getBounds());
+                    }
+                    catch (e) {
+                        //for points:
+                        bounds.extend(this.overlays[key].getCenter());
+                    }
+                }
+                this.map.fitBounds(bounds);
+            },
+
+            /** Gets the overlay from the overlay dictionary */
+            getOverlay: function (model) {
+                return this.overlays[model.id];
+            },
+            destroy: function () {
+                this.remove();
+            },
+            removeItem: function (model) {
+                delete this.overlays[model.id]
+            }
+        });
+        return localground.maps.views.OverlayGroup;
+    });
