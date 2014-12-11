90adba359c16783c3d64d7651ff1cb50c3f29dde
Zachary MacHardy <zmmachar@berkeley.edu>
2014-12-11 09:09:37 -0800

    reverting another whitespace change

--- static/backbone/js/api/base.js
+++ static/backbone/js/api/base.js
@@ -110,7 +110,7 @@ function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
         var cookies = document.cookie.split(';');
-        for ( var i = 0; i < cookies.length; i++) {
+        for (var i = 0; i < cookies.length; i++) {
             var cookie = jQuery.trim(cookies[i]);
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) == (name + '=')) {
