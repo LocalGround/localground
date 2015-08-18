47031b92a5dc90a9e027703bd26396fc3ddef1a4
Sarah Van Wart <vanwars@Sarahs-MacBook-Pro-3.local>
2015-08-17 18:44:01 -0700

    fixed broken Jasmine tests.

--- static/backbone/js/test/spec-helper.js
+++ static/backbone/js/test/spec-helper.js
@@ -181,8 +181,17 @@ define(
             this.app = _.extend({}, appUtilities);
             _.extend(this.app, {
                 vent: _.extend({}, Backbone.Events),
-                map: { fitBounds: function () {} } //a light stand-in for a Google Map, to speed it up; save our API calls.
+                activeProjectID: this.projects.models[0].id,
+                map: {
+                    fitBounds: function () {}
+                } //a light stand-in for a Google Map, to speed it up; save our API calls.
             });
+
+            this.spreadsheetApp = {
+                router: null,
+                activeTableID: null
+            };
+
             //initialize dataManager:
             this.dataManager = new DataManager({
                 app: this.app,
--- static/backbone/js/test/spec/views/photo-bubble-test.js
+++ static/backbone/js/test/spec/views/photo-bubble-test.js
@@ -1,30 +1,30 @@
 define(["underscore",
-        "jquery",
-        "lib/maps/overlays/infobubbles/photo",
         "lib/maps/overlays/photo",
         "config",
         "../../../test/spec-helper"],
-    function (_, $, PhotoBubble, PhotoOverlay, Config) {
+    function (_, PhotoOverlay, config) {
         'use strict';
 
         function initPhotoOverlay(scope) {
-                scope.app.setMap(new google.maps.Map('',{}));
-                //mock the getIcon method
-                spyOn(PhotoOverlay.prototype,'getIcon').and.callFake(function() {
-                    return {
-                        path: google.maps.SymbolPath.CIRCLE
-                    }
-                });
-                return new PhotoOverlay({
-                    app: scope.app,
-                    model: scope.photos.models[0],
-                    infoBubbleTemplates: {
-                        InfoBubbleTemplate: _.template(Config['photos'].InfoBubbleTemplate),
-                        TipTemplate: _.template(Config['photos'].TipTemplate)
-                    },
-                    
-                });
-        }   
+            scope.app.map = new google.maps.Map(document.getElementById('map_canvas'), {
+                center: { lat: -34, lng: 150 }
+            });
+
+            //mock the getIcon method
+            spyOn(PhotoOverlay.prototype, 'getIcon').and.callFake(function () {
+                return {
+                    path: google.maps.SymbolPath.CIRCLE
+                };
+            });
+            return new PhotoOverlay({
+                app: scope.app,
+                model: scope.photos.models[0],
+                infoBubbleTemplates: {
+                    InfoBubbleTemplate: _.template(config.photos.InfoBubbleTemplate),
+                    TipTemplate: _.template(config.photos.TipTemplate)
+                }
+            });
+        }
 
         describe("PhotoBubble", function () {
             var photoOverlay;
@@ -35,9 +35,5 @@ define(["underscore",
                 photoOverlay.showBubble();
                 expect(photoOverlay.infoBubble.$el.find('.edit-photo-container > img')[0].outerHTML).toEqual('<img src="//:0">');
             });
-
-
-        }); 
-
-
+        });
     });
