38fe4c1ac011238c7d7ee1679f21b8deaa013181
Scot <scot.spinner@gmail.com>
2016-02-08 14:38:58 -0800

    updated to use django db for task backend

--- apps/tasks.py
+++ apps/tasks.py
@@ -1,10 +1,17 @@
 from celery import Celery
 from django.conf import settings
-import os
+import os, sys
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-app = Celery('tasks', backend='rpc://guest@localhost//', broker='amqp://guest@localhost//')
-#app = Celery('tasks', backend='djcelery.backends.database:DatabaseBackend', broker='amqp://guest@localhost//')
+cwd = os.path.dirname(os.path.realpath(__file__))
+apps = os.path.dirname(cwd)
+workspace = os.path.dirname(apps)
+sys.path.append(workspace) 
+
+#import localground.apps.settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localground.apps.settings')
+#app = Celery('tasks', backend='rpc://guest@localhost//', broker='amqp://guest@localhost//')
+app = Celery('tasks', backend='djcelery.backends.database:DatabaseBackend', broker='amqp://guest@localhost//')
 #app.conf.update(
 #    CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
 #)
