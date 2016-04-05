4dbbf64fa9b22c78732a809c7de1900cc89acaab
Scot <scot.spinner@gmail.com>
2016-04-05 12:18:06 -0700

    got the userprofile endpoint to work as is currently setup

--- apps/site/api/serializers/user_profile_serializer.py
+++ apps/site/api/serializers/user_profile_serializer.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from localground.apps.site import models
 from localground.apps.site.api import fields
 from django.contrib.auth.models import User
-
+from localground.apps.site.models.permissions import ObjectAuthority
 
 class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -21,12 +21,15 @@ class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
             'time_stamp')
         depth = 1
 
-    id = serializers.IntegerField(required=True, label='id')
+    id = serializers.IntegerField(
+        label='id',
+        read_only=True)
     email_announcements = serializers.NullBooleanField(
         required=False,
         label='email_announcements')
     default_location = fields.GeometryField(
         required=False,
+        allow_null=True,
         label='default_location')
     time_stamp = serializers.DateTimeField(required=False, label='time_stamp')
     user = serializers.HyperlinkedRelatedField(
@@ -40,31 +43,55 @@ class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     date_created = serializers.DateTimeField(
         required=False,
         label='date_created')
-    default_view_authority = serializers.CharField(
+    default_view_authority = serializers.ChoiceField(
+        choices=map(lambda x: (x.name, x.name),ObjectAuthority.objects.all()),
         required=False,
         label='default_view_authority')
 
-    def restore_object(self, attrs, instance=None):
-        """
-        Given a dictionary of deserialized field values, either update
-        an existing model instance, or create a new model instance.
-        """
-        if instance is not None:
-            instance.id = attrs.get('id', instance.id)
-            instance.email_announcements = attrs.get(
-                'email_announcements',
-                instance.email_announcements)
-            instance.default_location = attrs.get(
-                'default_location',
-                instance.default_location)
-            instance.time_stamp = attrs.get('time_stamp', instance.time_stamp)
-            instance.user = attrs.get('user', instance.user)
-            #instance.contacts = attrs.get('contacts', instance.contacts)
-            instance.date_created = attrs.get(
-                'date_created',
-                instance.date_created)
-            instance.default_view_authority = attrs.get(
-                'default_view_authority',
-                instance.default_view_authority)
-            return instance
-        return models.UserProfile(**attrs)
+    # def restore_object(self, attrs, instance=None):
+    #     """
+    #     Given a dictionary of deserialized field values, either update
+    #     an existing model instance, or create a new model instance.
+    #     """
+    #     if instance is not None:
+    #         instance.id = attrs.get('id', instance.id)
+    #         instance.email_announcements = attrs.get(
+    #             'email_announcements',
+    #             instance.email_announcements)
+    #         instance.default_location = attrs.get(
+    #             'default_location',
+    #             instance.default_location)
+    #         instance.time_stamp = attrs.get('time_stamp', instance.time_stamp)
+    #         instance.user = attrs.get('user', instance.user)
+    #         #instance.contacts = attrs.get('contacts', instance.contacts)
+    #         instance.date_created = attrs.get(
+    #             'date_created',
+    #             instance.date_created)
+    #         instance.default_view_authority = attrs.get(
+    #             'default_view_authority',
+    #             instance.default_view_authority)
+    #         return instance
+    #     return models.UserProfile(**attrs)
+    def update(self, instance, validated_data):
+        instance.id = validated_data.get('id', instance.id)
+        instance.email_announcements = validated_data.get(
+            'email_announcements',
+            instance.email_announcements)
+        instance.default_location = validated_data.get(
+            'default_location',
+            instance.default_location)
+        instance.time_stamp = validated_data.get('time_stamp', instance.time_stamp)
+        instance.user = validated_data.get('user', instance.user)
+        instance.date_created = validated_data.get(
+            'date_created',
+            instance.date_created)
+        instance.default_view_authority = ObjectAuthority.objects.get(name=validated_data.get('default_view_authority', instance.default_view_authority))
+        # validated_data.get(
+        #     'default_view_authority',
+        #     instance.default_view_authority)
+        instance.save()
+        return instance
+    def create(self, validated_data):
+        return models.UserProfile.objects.create(**validated_data)
+
+
