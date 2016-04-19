30db6f487b9c69a7441792e6498478544ef9f816
Sarah <vanwars@gmail.com>
2016-04-19 11:48:24 -0700

    UserProfile update works

--- apps/site/api/serializers/user_profile_serializer.py
+++ apps/site/api/serializers/user_profile_serializer.py
@@ -4,55 +4,74 @@ from localground.apps.site import models
 from localground.apps.site.api import fields
 from django.contrib.auth.models import User
 from localground.apps.site.models.permissions import ObjectAuthority
+from localground.apps.site.api.serializers.base_serializer import AuditSerializerMixin
 
-class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
+#class UserProfileSerializer(serializers.HyperlinkedModelSerializer):   
+class UserProfileSerializer(AuditSerializerMixin, serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = models.UserProfile
         fields = (
             'url',
             'id',
-            'user',
             'first_name',
             'last_name',
+            'email',
+            'username',
             'email_announcements',
             'default_view_authority',
             'default_location',
             'contacts',
             'date_created',
-            'time_stamp')
+            'time_stamp',
+            'user'
+        )
+        read_only_fields = ('date_created', 'time_stamp')#, 'user')
         depth = 1
-
-    id = serializers.IntegerField(
-        label='id',
-        read_only=True)
+        
+    user = serializers.HyperlinkedRelatedField(
+        label='user',
+        view_name='user-detail',
+        read_only=True
+    )
+        
     email_announcements = serializers.NullBooleanField(
         required=False,
         label='email_announcements')
+
     default_location = fields.GeometryField(
         required=False,
         allow_null=True,
         label='default_location')
-    time_stamp = serializers.DateTimeField(required=False, label='time_stamp')
-    user = serializers.HyperlinkedRelatedField(
-        label='user',
-        view_name='user-detail',
-        read_only=True)
+
     first_name = serializers.CharField(
         required=False,
+        allow_blank=True,
         label='first_name',
         source='user.first_name')
+
     last_name = serializers.CharField(
         required=False,
+        allow_blank=True,
         label='last_name',
         source='user.last_name')
+    
+    email = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        label='email',
+        source='user.email')
+    
+    username = serializers.CharField(
+        read_only=True,
+        label='username',
+        source='user.username')
+
     contacts = serializers.RelatedField(
         many=True,
         label='contacts',
         read_only=True)
-    date_created = serializers.DateTimeField(
-        required=False,
-        label='date_created')
+
     default_view_authority = serializers.ChoiceField(
         choices=map(lambda x: (x.name, x.name),ObjectAuthority.objects.all()),
         required=False,
@@ -82,6 +101,19 @@ class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     #             instance.default_view_authority)
     #         return instance
     #     return models.UserProfile(**attrs)
+    
+    def update(self, instance, validated_data):
+        validated_data.update(self.get_presave_create_dictionary())
+        user = instance.user
+        updated_user = validated_data.get('user')
+        user.first_name = updated_user.get('first_name', user.first_name)
+        user.last_name = updated_user.get('last_name', user.last_name)
+        user.email = updated_user.get('email', user.email)
+        user.save()
+        instance.save()
+        return instance
+    
+    '''
     def update(self, instance, validated_data):
         instance.id = validated_data.get('id', instance.id)
         instance.email_announcements = validated_data.get(
@@ -101,7 +133,5 @@ class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
         instance.user.objects.last_name = validated_data.get('last_name')
         instance.user.objects.save()
         return instance
-    def create(self, validated_data):
-        return models.UserProfile.objects.create(**validated_data)
-
+    '''
 
