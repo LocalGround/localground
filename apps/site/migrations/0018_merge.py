9c40c05fe90509aa0d3d2ecfa48390c296e4f2a3
Sarah <vanwars@gmail.com>
2016-05-04 08:35:19 -0700

    fixing broken migration file

--- apps/site/migrations/0018_merge.py
+++ apps/site/migrations/0018_merge.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('site', '0017_auto_20160320_2124'),
+        #('site', '0017_auto_20160320_2124'),
         ('site', '0014_auto_20160322_1631'),
     ]
 
