4be9b7ae8a47dc510556e0ec815b9923d40cbd9b
John Peter McGrath <mrjbrpg@gmail.com>
2016-11-08 09:32:57 -0800

    Added in rough functionality for Modal window when pressing button

--- static/redesign/apps/home/views/main.js
+++ static/redesign/apps/home/views/main.js
@@ -21,7 +21,33 @@ define(["marionette",
                         'click .action': 'showModal'
                     },
                     showModal: function () {
-                        alert("Show create project text box");
+                        //alert("Show share project modal form");
+                        
+                        // Get the modal
+                        var modal = document.getElementById('myModal');
+
+                        // Get the button that opens the modal
+                        var btn = document.getElementById("add-project");
+
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+
+                        // When the user clicks the button, open the modal
+                        btn.onclick = function() {
+                            modal.style.display = "block";
+                        }
+
+                        // When the user clicks on <span> (x), close the modal
+                        span.onclick = function() {
+                            modal.style.display = "none";
+                        }
+
+                        // When the user clicks anywhere outside of the modal, close it
+                        window.onclick = function(event) {
+                            if (event.target == modal) {
+                                modal.style.display = "none";
+                            }
+                        }
                     }
                 });
             },
@@ -32,19 +58,17 @@ define(["marionette",
             searchTerm: null,
             childViewContainer: "#gallery-main",
             events: {
-                'click #search': 'doSearch',
-                'click #clear': 'clearSearch'
+                'click #add-project': 'addProject',
+                'click #search': 'doSearch'
+            },
+            addProject: function () {
+                alert("Show create project modal form");
             },
             template: Handlebars.compile(ListTemplate),
-            templateHelpers: function () {
-                return {
-                    searchTerm: this.searchTerm
-                };
-            },
+
             initialize: function (opts) {
                 _.extend(this, opts);
 
-                // call Marionette's default functionality (similar to "super")
                 Marionette.CompositeView.prototype.initialize.call(this);
 
                 this.displayProjects();
@@ -52,27 +76,37 @@ define(["marionette",
                 // when the fetch completes, call Backbone's "render" method
                 // to create the gallery template and bind the data:
                 this.listenTo(this.collection, 'reset', this.render);
+
+                //listen to events that fire from other parts of the application:
+                this.listenTo(this.app.vent, 'search-requested', this.doSearch);
+                this.listenTo(this.app.vent, 'clear-search', this.clearSearch);
             },
             hideLoadingMessage: function () {
                 this.$el.find(this.childViewContainer).empty();
             },
 
-            getSearchString: function () {
-                this.searchTerm = this.$el.find("#project-search").val();
-                return "name like %" + this.searchTerm +
-                        "% OR caption like %" + this.searchTerm +
-                        "%";
+            getDefaultQueryString: function () {
+                return "WHERE project = " + this.app.selectedProject.id;
             },
-
+            templateHelpers: function () {
+                return {
+                    searchTerm: this.searchTerm
+                };
+            },
             doSearch: function (e) {
-                this.collection.query = "WHERE " + this.getSearchString();
-                this.collection.fetch({ reset: true });
                 e.preventDefault();
+                var term = this.$el.find("#searchTerm").val(),
+                    query = "WHERE name like %" + term +
+                            "% OR caption like %" + term +
+                            "% OR owner like %" + term +
+                            "% OR tags contains (" + term + ")";
+                this.searchTerm = term;
+                this.collection.query = query;
+                this.collection.fetch({ reset: true });
             },
 
             clearSearch: function (e) {
-                this.searchTerm = null;
-                this.collection.query = "";
+                this.collection.query = this.getDefaultQueryString();
                 this.collection.fetch({ reset: true });
                 e.preventDefault();
             },
