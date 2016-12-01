c90481af2ba3e61e8ca51fea73091fb7a368400e
John Peter McGrath <mrjbrpg@gmail.com>
2016-12-01 10:02:17 -0800

    Added in a create form js file and is currently being set up

---
+++ static/redesign/apps/gallery/views/create-form.js
@@ -0,0 +1,11 @@
+define([
+  "underscore",
+  "jquery",
+  "handlebars",
+  "marionette",
+  "text!../templates/create-form.html"
+],
+  function (_, $, Handlebars, Marionette, CreateFormTemplate){
+    // Setting up a create form js
+  }
+);
