2a5670197a177bf3cac7cc6519553378d52d7c2c
John Peter McGrath <mrjbrpg@gmail.com>
2016-12-02 13:02:33 -0800

    Added more stuff to prepare for further development for the create form modal and to eventually get the fields part up and running

--- static/redesign/apps/gallery/views/create-form.js
+++ static/redesign/apps/gallery/views/create-form.js
@@ -24,6 +24,21 @@ define([
 
         initialize: function (opts) {
             _.extend(this, opts);
+
+            if (this.model == undefined){
+              // Create a blank project if new project made
+              this.model = new Form();
+            } else {
+
+              //this.collection = this.model.projectUsers;
+              Marionette.CompositeView.prototype.initialize.call(this);
+              //this.model.getProjectUsers();
+              //this.listenTo(this.collection, 'reset', this.render);
+              //this.listenTo(this.collection, 'destroy', this.render);
+            }
+
+            //this.listenTo(this.model, 'sync', this.createNewProjectUsers);
+
             Marionette.ItemView.prototype.initialize.call(this);
             this.template = Handlebars.compile(CreateFormTemplate);
             this.render();
@@ -32,10 +47,60 @@ define([
             this.$el.hide();
         },
 
+
+        /*
+          Behavior problem: After opeiing the modal window and closing
+          for the first time, modal windows opo up when
+          clicking on the add button again.
+        */
+        onRender: function () {
+          //console.log("rerender");
+          var modal = this.$el.find('.modal').get(0),
+          span = this.$el.find('.close').get(0);
+          modal.style.display = "block";
+          // When the user clicks on <span> (x), close the modal
+          span.onclick = function () {
+            modal.style.display = "none";
+          }
+
+          // When the user clicks anywhere outside of the modal, close it
+          window.onclick = function (event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          }
+        },
+
+        templateHelpers: function () {
+          /*
+          // This will eventually consider the number of fields represented
+          // in columns for the tags and values while the rows contains
+          // the data stored into the tags
+          if (this.model.projectUsers == undefined) return;
+          return {
+            projectUsers: this.model.projectUsers.toJSON()
+          };
+          */
+        },
+
         // Need to add more functions to handle various events
         // and to get the form to open up
         saveFormSettings: function () {
             alert("Need to save form");
+
+            // Gather the html data
+            var $formName = $('#formName').val();
+            var $shareType = $('#share_type').val();
+            var $caption = $('#caption').val();
+            var $tags = $('#tags').val();
+
+            this.model.set('name', $formName);
+            this.model.set('access_authority', $shareType);
+            this.model.set('tags', $tags);
+            this.model.set('caption', $caption);
+            this.model.set('slug', 'slug_' + parseInt(Math.random()*100000));
+            this.model.set('project_ids', [this.app.selectedProject.id]);
+            this.model.save();
         }
 
     });
--- static/redesign/models/form.js
+++ static/redesign/models/form.js
@@ -1,6 +1,40 @@
-define(["models/base"], function (Base) {
+define(
+  ["underscore", "models/base", "models/field", "collections/fields"],
+  function (_, Base, Field, Fields) {
     "use strict";
     var Form = Base.extend({
+      defaults: _.extend({}, Base.prototype.defaults, {
+          isActive: false,
+          isVisible: false,
+          checked: false
+      }),
+      url: '/api/0/forms/',
+
+      initialize: function (data, opts) {
+          if (this.get("id")) {
+              this.fields = new Fields(null, { id: this.get("id") });
+          }
+          Base.prototype.initialize.apply(this, arguments);
+      },
+
+      getFields: function(){
+        this.fields.fetch({ reset: true });
+      },
+
+      // This is a rough draft of the following function
+      // that is expected to change throughout revision
+      shareWithUser: function (fieldType, authorityID) {
+          var field = new Field(null, { id: this.get("id") }),
+              that = this;
+          projectUser.set("user", fieldType);
+          projectUser.set("authority", authorityID);
+          projectUser.save(null, {
+              success: function () {
+                  that.getFields();
+              }
+          });
+      }
+
     });
     return Form;
 });
