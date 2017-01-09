a30709ebe9003c54d76b7650d1f00e2a81f18ed8
code510 <code510@calvisitor-10-105-157-227.calvisitor.1918.berkeley.edu>
2017-01-09 10:29:38 -0800

    new files added

---
+++ static/redesign/apps/style/views/skin-view.js
@@ -0,0 +1,26 @@
+define(["marionette",
+        "handlebars",
+        "collections/maps",
+        "text!../templates/skin.html"
+    ],
+    function (Marionette, Handlebars, Maps, SkinTemplate) {
+        'use strict';
+
+        var SelectSkinView = Marionette.ItemView.extend({
+
+            template: Handlebars.compile(SkinTemplate),
+
+            initialize: function (opts) {
+                this.app = opts.app;
+
+                // here is some fake data until the
+                // /api/0/maps/ API Endpoint gets built:
+                this.collection = new Maps([
+                    { id: 1, name: "Greyscale", project_id: 4 },
+                    { id: 2, name: "Default", project_id: 4 },
+                    { id: 3, name: "Dark", project_id: 4 }                ]);
+            }
+
+        });
+        return SelectSkinView;
+    });
