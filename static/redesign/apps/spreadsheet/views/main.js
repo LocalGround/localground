682e672efc1a0d5c0e81cc05ffaad248d2436003
John Peter McGrath <mrjbrpg@gmail.com>
2017-01-09 13:54:08 -0800

    Now Add row works, but needs little polishing

--- static/redesign/apps/spreadsheet/views/main.js
+++ static/redesign/apps/spreadsheet/views/main.js
@@ -354,9 +354,13 @@ define(["marionette",
                 rec.collection = this.collection;
                 rec.save();
                 this.collection.add(rec);
+                this.renderSpreadsheet();
+
+                // GOtta do something about the order of ID on table
+
                 // Trying to add a row to the handsonTable at the end index,
                 // but I have no luck yet with that alone
-                this.table.alter("insert_row", null);
+                //this.table.alter("insert_row", null);
 
             }
 
