f2a93418107a5b032ec4e603e8d3bb41c5ee3b00
Sarah <vanwars@gmail.com>
2017-01-10 11:39:49 -0800

    bugfix for default panel styles in map.js model

--- static/redesign/models/map.js
+++ static/redesign/models/map.js
@@ -4,17 +4,17 @@ define(["models/base"], function (Base) {
         getNamePlural: function () {
             return "maps";
         },
-        defaults: _.extend({}, Base.prototype.defaults, {
-            checked: false,
-            panel_styles: JSON.parse(JSON.stringify(
-                [
-                {type: "title", font: "Lato", fw: "bold", color: "black", size: "15"},
-                {type: "subtitle", font: "Lato", fw: "regular", color: "gray", size: "12"},
-                {type: "paragraph", font: "Lato", fw: "regular", color: "black", size: "12"},
-                {type: "tags", font: "Lato", fw: "regular", color: "black", size: "10"},    
+        defaults: function () {
+            return _.extend({}, Base.prototype.defaults, {
+                checked: false,
+                panel_styles: [
+                    {type: "title", font: "Lato", fw: "bold", color: "black", size: "15"},
+                    {type: "subtitle", font: "Lato", fw: "regular", color: "gray", size: "12"},
+                    {type: "paragraph", font: "Lato", fw: "regular", color: "black", size: "12"},
+                    {type: "tags", font: "Lato", fw: "regular", color: "black", size: "10"}
                 ]
-            ))
-        })
+            });
+        }
     });
     return Map;
 });
