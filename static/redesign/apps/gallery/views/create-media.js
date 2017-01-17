560c3ecc3bc500237e35f2dfe18e3f1024c028d8
John Peter McGrath <mrjbrpg@gmail.com>
2017-01-17 08:46:47 -0800

    Got started on create media code

---
+++ static/redesign/apps/gallery/views/create-media.js
@@ -0,0 +1,69 @@
+define([
+    "backbone",
+    "marionette",
+    "handlebars",
+    "models/field",
+    "text!../templates/create-field.html",
+    "form"
+], function (Backbone, Marionette, Handlebars, Field, CreateFieldTemplate) {
+    'use strict';
+
+    /*
+      As of now, this is a rough draft copy-paste for
+      creating and uploading media and subject to further changes
+    */
+    var CreateMediaView = Marionette.ItemView.extend({
+        template: Handlebars.compile(CreateFieldTemplate),
+        initialize: function (opts) {
+            this.model = new Field(null, { id: opts.formID });
+            this.fields = opts.fields;
+            this.app = opts.app;
+            // see documentation: https://github.com/powmedia/backbone-forms
+            this.form = new Backbone.Form({
+                model: this.model,
+                schema: {
+                    col_alias: {
+                        type: 'Text',
+                        title: "Name",
+                        validators: ['required']
+                    },
+                    data_type: {
+                        title: 'Media Type',
+                        type: 'Select',
+                        options: { audio: 'Audio', photos: 'Photo'}
+                    }
+                }
+            }).render();
+            this.render();
+            this.$el.find("#model-form").append(this.form.$el);
+        },
+        saveToDatabase: function () {
+            var that = this;
+            // see the "saveModel" method of the
+            // apps/gallery/views/data-detail.js to see
+            // how to save, using Backbone forms
+            var errors = this.form.commit({ validate: true });
+            if (errors) {
+                console.log("errors: ", errors);
+                return;
+            }
+            //alert("save");
+            this.model.set("ordering", this.fields.length+1); // list starting at 1, thus +1 needed
+            this.model.save(
+                null,
+                {
+                    success: function(){
+                        // Successfully add a new field
+                        that.fields.add(that.model);
+                        that.app.vent.trigger("render-spreadsheet");
+                        that.app.vent.trigger("hide-modal");
+                    }
+                }
+            );
+
+        }
+
+    });
+    return CreateMediaView;
+
+});
