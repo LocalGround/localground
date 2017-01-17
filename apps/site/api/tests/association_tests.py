1cccdf2cd4f798b98a1f0fbae96d56af901e5843
Sarah <vanwars@gmail.com>
2017-01-16 19:27:16 -0800

    association tests now working

--- apps/site/api/tests/association_tests.py
+++ apps/site/api/tests/association_tests.py
@@ -109,7 +109,6 @@ class ApiRelatedMediaListTest(
             '/api/0/forms/%s/data/%s/%s/' % (self.form.id, self.record.id, source_type): r1.id
         }
         for url in urls:
-            print url
             response = self.client_user.post(url, {
                     'object_id': urls[url],
                     'ordering': 1
@@ -145,95 +144,110 @@ class ApiRelatedMediaInstanceTest(
         self.photo2 = self.create_photo(self.user, self.project)
         self.audio1 = self.create_audio(self.user, self.project)
         self.audio2 = self.create_audio(self.user, self.project)
-        self.create_relation(models.Photo.get_content_type(), id=self.photo1.id, ordering=1)
-        self.create_relation(models.Audio.get_content_type(), id=self.audio1.id, ordering=1)
         
         # create urls:
-        url = '/api/0/markers/%s/%s/%s/'
         self.urls = [
-            url % (self.marker.id, 'photos', self.photo1.id),
-            url % (self.marker.id, 'audio', self.audio1.id)
+            '/api/0/markers/%s/%s/%s/' % (self.marker.id, 'photos', self.photo1.id),
+            '/api/0/markers/%s/%s/%s/' % (self.marker.id, 'audio', self.audio1.id),
+            '/api/0/forms/%s/data/%s/%s/%s/' % (self.form.id, self.record.id, 'photos', self.photo1.id),
+            '/api/0/forms/%s/data/%s/%s/%s/' % (self.form.id, self.record.id, 'audio', self.audio1.id)
         ]
         
+        # create associations
+        self.create_relation(self.marker, self.photo1)
+        self.create_relation(self.marker, self.audio1)
+        self.create_relation(self.record, self.photo1)
+        self.create_relation(self.record, self.audio1)
+        
+    def tearDown(self):
+        # delete associations:
+        self.delete_relation(self.marker, self.photo1)
+        self.delete_relation(self.marker, self.audio1)
+        self.delete_relation(self.record, self.photo1)
+        self.delete_relation(self.record, self.audio1)
+        
+        # delete custom forms:
+        for m in models.Form.objects.all():
+            m.remove_table_from_cache()
         
     def test_page_200_status_basic_user(self, **kwargs):
-        url = '/api/0/markers/%s/%s/%s/'
-        urls = [
-            url % (self.marker.id, 'photos', self.photo1.id),
-            url % (self.marker.id, 'audio', self.audio1.id)
-        ]
-        ViewMixinAPI.test_page_200_status_basic_user(self, urls=urls)
+        ViewMixinAPI.test_page_200_status_basic_user(self)
 
     def test_page_resolves_to_view(self):
-        url = '/api/0/markers/%s/%s/%s/'
-        urls = [
-            url % (self.marker.id, 'photos', self.photo1.id),
-            url % (self.marker.id, 'audio', self.audio1.id)
-        ]
-        ViewMixinAPI.test_page_resolves_to_view(self, urls=urls)
+        ViewMixinAPI.test_page_resolves_to_view(self)
 
     def test_remove_media_from_marker(self, **kwargs):
-        '''
-        Important note:  the HTTP_X_CSRFTOKEN header must be set for
-        a successful delete using Session Authentication
-        (had to dig through Django's middleware/csrf.py to sort that out)
-        '''
-        source_type = models.Marker.get_content_type()
-        url = '/api/0/markers/%s/%s/'
-        for i, url in enumerate([ url % (self.marker.id, 'photos'), url % (self.marker.id, 'audio') ]):
-            entity_type = models.Base.get_model(
-                model_name_plural=url.split('/')[-2]
-            ).get_content_type()
-
-            # 0)  Attach media to marker
-            entity_id = self.photo2.id
-            self.create_relation(entity_type, id=entity_id)
-
+        
+        urls = {
+            '/api/0/markers/%s/photos/%s/' % (self.marker.id, self.photo1.id): {
+                'source_model': self.marker, 'attach_model': self.photo1    
+            },
+            '/api/0/forms/%s/data/%s/photos/%s/' % (self.form.id, self.record.id, self.photo1.id): {
+                'source_model': self.record, 'attach_model': self.photo1    
+            },
+            '/api/0/markers/%s/audio/%s/' % (self.marker.id, self.audio1.id): {
+                'source_model': self.marker, 'attach_model': self.audio1    
+            },
+            '/api/0/forms/%s/data/%s/audio/%s/' % (self.form.id, self.record.id, self.audio1.id): {
+                'source_model': self.record, 'attach_model': self.audio1    
+            }
+        }
+        for url in urls:
             # 1) make sure that the object is appended to the marker:
+            source_model = urls[url]['source_model']
+            attach_model = urls[url]['attach_model']
+            
+            #self.create_relation(source_model, attach_model)
+        
             queryset = models.GenericAssociation.objects.filter(
-                entity_type=entity_type,
-                entity_id=entity_id,
-                source_type=source_type,
-                source_id=self.marker.id,
+                entity_type=type(attach_model).get_content_type(),
+                entity_id=attach_model.id,
+                source_type=type(source_model).get_content_type(),
+                source_id=source_model.id
             )
             self.assertEqual(len(queryset), 1)
 
             # 2) remove object from marker:
-            response = self.client_user.delete('%s%s/' % (url, entity_id),
-                                               HTTP_X_CSRFTOKEN=self.csrf_token
-                                               )
+            response = self.client_user.delete(url, HTTP_X_CSRFTOKEN=self.csrf_token)
+
             # print response.content
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
             # 3) ensure object has been removed from the marker:
             # have to re-instantiate the query b/c it's cached:
             queryset = models.GenericAssociation.objects.filter(
-                entity_type=entity_type,
-                entity_id=entity_id,
-                source_type=source_type,
-                source_id=self.marker.id,
+                entity_type=type(attach_model).get_content_type(),
+                entity_id=attach_model.id,
+                source_type=type(source_model).get_content_type(),
+                source_id=source_model.id
             )
             self.assertEqual(len(queryset), 0)
             
     def _test_using_put_or_patch(self, f, params, **kwargs):
-        source_type = models.Marker.get_content_type()
-        url = '/api/0/markers/%s/%s/'
-        for i, url in enumerate([ url % (self.marker.id, 'photos'), url % (self.marker.id, 'audio') ]):
-            entity_type = models.Base.get_model(
-                model_name_plural=url.split('/')[-2]
-            ).get_content_type()
-
-            # Attach media to marker
-            if entity_type.name == "photo":
-                entity_id = self.photo2.id
-            else:
-                entity_id = self.audio2.id
+        urls = {
+            '/api/0/markers/%s/photos/%s/' % (self.marker.id, self.photo1.id): {
+                'source_model': self.marker, 'attach_model': self.photo1    
+            },
+            '/api/0/forms/%s/data/%s/photos/%s/' % (self.form.id, self.record.id, self.photo1.id): {
+                'source_model': self.record, 'attach_model': self.photo1    
+            },
+            '/api/0/markers/%s/audio/%s/' % (self.marker.id, self.audio1.id): {
+                'source_model': self.marker, 'attach_model': self.audio1    
+            },
+            '/api/0/forms/%s/data/%s/audio/%s/' % (self.form.id, self.record.id, self.audio1.id): {
+                'source_model': self.record, 'attach_model': self.audio1    
+            }
+        }
+        for url in urls:
+            # 1) make sure that the object is appended to the marker:
+            source_model = urls[url]['source_model']
+            attach_model = urls[url]['attach_model']
+        
+            relation = self.get_relation(source_model, attach_model)
                 
-            relation = self.create_relation(entity_type, id=entity_id)
             self.assertEqual(relation.ordering, 1)
             self.assertEqual(relation.turned_on, False)
             import urllib
-            url = '%s%s/' % (url, entity_id)
             response = f(
                 url,
                 data=urllib.urlencode(params),
