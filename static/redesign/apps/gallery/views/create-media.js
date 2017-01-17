bb47c12c289536585cde5541a7398ee16ed17ac2
Sarah <vanwars@gmail.com>
2017-01-17 12:46:39 -0800

    file upload manager files

--- static/redesign/apps/gallery/views/create-media.js
+++ static/redesign/apps/gallery/views/create-media.js
@@ -2,8 +2,11 @@ define([
     "backbone",
     "marionette",
     "handlebars",
-    "text!../templates/create-media.html"
-], function (Backbone, Marionette, Handlebars, CreateMediaTemplate) {
+    "text!../templates/create-media.html",
+    "text!../templates/uploader/upload-manager.file.html",
+    "text!../templates/uploader/upload-manager.main.html",
+    "backbone-upload-manager"
+], function (Backbone, Marionette, Handlebars, CreateMediaTemplate, MainTemplate, FileTemplate) {
     'use strict';
 
     /*
@@ -16,7 +19,18 @@ define([
         initialize: function (opts) {
             _.extend(this, opts);
             console.log(this.template);
+            
+            var uploadManager = new Backbone.UploadManager({
+	            uploadUrl: '/api/0/photos/',
+	            templates: {
+	                main: "../static/redesign/apps/gallery/templates/uploader/upload-manager.main.html",
+	                file: "../static/redesign/apps/gallery/templates/uploader/upload-manager.file.html"
+	            }
+	        });
+            console.log(uploadManager);
             this.render();
+            //this.$el.append(uploadManager.$el);
+            uploadManager.renderTo(this.$el);
         }
     });
     return CreateMediaView;
