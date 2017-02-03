f51cd1a19bb996c3e9eea00f241c7a42b75c4595
Sarah <vanwars@gmail.com>
2017-02-02 21:03:33 -0800

    finished CSS for carousel

--- static/redesign/lib/carousel/carousel.js
+++ static/redesign/lib/carousel/carousel.js
@@ -4,38 +4,65 @@ define(["jquery", "underscore", "marionette", "handlebars", "text!../carousel/ca
         var Carousel = Marionette.ItemView.extend({
             events: {
                 "click .next": "next",
-                "click .prev": "prev"
+                "click .prev": "prev",
+                "click .show-slide": "jump"
             },
             counter: 0,
             template: Handlebars.compile(CarouselTemplate),
             initialize: function (opts) {
-                opts = opts || {};
                 _.extend(this, opts);
                 this.render();
                 this.$el.addClass('active-slide');
                 this.navigate(0);
             },
+            numPhotos: function () {
+                var children = this.model.get("children");
+                if (children && children.photos && children.photos.data) {
+                    return children.photos.data.length;
+                }
+                return 0;
+            },
+            templateHelpers: function () {
+                return {
+                    num_photos: this.numPhotos()
+                };
+            },
 
-            navigate: function (direction) {
+            navigate: function () {
                 var $items = this.$el.find('.carousel-content li'),
                     amount = $items.length;
                 $items.removeClass('current');
-                this.counter += direction;
                 if (this.counter < 0) {
                     this.counter = amount - 1;
                 }
                 if (this.counter >= amount) {
                     this.counter = 0;
                 }
+                this.updateCircles();
                 $($items[this.counter]).addClass('current');
             },
 
             next: function () {
-                this.navigate(1);
+                this.counter += 1;
+                this.navigate();
             },
 
             prev: function () {
-                this.navigate(-1);
+                this.counter -= 1;
+                this.navigate();
+            },
+
+            updateCircles: function () {
+                var $items = this.$el.find('.show-slide'),
+                    $clicked = $($items[this.counter]);
+                $items.removeClass("fa-circle");
+                $items.not($clicked).addClass("fa-circle-o");
+                $clicked.addClass("fa-circle");
+            },
+
+            jump: function (e) {
+                this.counter = parseInt($(e.target).attr("data-index"), 10);
+                this.navigate();
             }
         });
         return Carousel;
