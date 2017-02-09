e9cbb2f777a2c50aff80145a010b1e6a5da113a5
Sarah <vanwars@gmail.com>
2017-02-09 15:38:43 -0800

    more carousel stuff

--- static/redesign/lib/carousel/carousel.js
+++ static/redesign/lib/carousel/carousel.js
@@ -1,8 +1,11 @@
 define(["jquery", "underscore", "marionette", "handlebars",
-        "text!../carousel/carousel-photo.html", "text!../carousel/carousel-audio.html"],
-    function ($, _, Marionette, Handlebars, CarouselPhotoTemplate, CarouselAudioTemplate) {
+        "collections/photos", "collections/audio",
+        "text!../carousel/carousel-photo.html", "text!../carousel/carousel-audio.html",
+        "text!../carousel/carousel-photo-item.html"],
+    function ($, _, Marionette, Handlebars, Photos, Audio,
+              CarouselPhotoTemplate, CarouselAudioTemplate, PhotoItemTemplate) {
         'use strict';
-        var Carousel = Marionette.ItemView.extend({
+        var Carousel = Marionette.CompositeView.extend({
             events: {
                 "click .next": "next",
                 "click .prev": "prev",
@@ -10,27 +13,34 @@ define(["jquery", "underscore", "marionette", "handlebars",
             },
             counter: 0,
             mode: "photos",
+            childViewContainer: ".carousel-content",
             initialize: function (opts) {
                 _.extend(this, opts);
                 if (this.mode == "photos") {
                     this.template = Handlebars.compile(CarouselPhotoTemplate);
+                    this.collection = new Photos(this.model.get("children").photos.data);
                 } else {
                     this.template = Handlebars.compile(CarouselAudioTemplate);
+                    this.collection = new Audio(this.model.get("children").audio.data);
                 }
+                console.log(this.collection);
                 this.render();
                 this.$el.addClass('active-slide');
                 this.navigate(0);
             },
-            numChildren: function (key) {
-                var children = this.model.get("children");
-                if (children && children[key] && children[key].data) {
-                    return children[key].data.length;
-                }
-                return 0;
+            getChildView: function () {
+                return Marionette.ItemView.extend({
+                    initialize: function (opts) {
+                        _.extend(this, opts);
+                        this.template = Handlebars.compile(PhotoItemTemplate);
+                    },
+                    tagName: "li"
+                });
             },
+
             templateHelpers: function () {
                 return {
-                    num_children: this.numChildren(this.mode),
+                    num_children: this.collection.length,
                     isSpreadsheet: this.app.screenType === "spreadsheet"
                 };
             },
@@ -38,7 +48,7 @@ define(["jquery", "underscore", "marionette", "handlebars",
             navigate: function () {
                 console.log('navigate');
                 var $items = this.$el.find('.carousel-content li'),
-                    amount = $items.length;
+                    amount = this.collection.length;
                 $items.removeClass('current');
                 console.log($items);
                 if (this.counter < 0) {
