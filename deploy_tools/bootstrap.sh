b2f37ec1a32406a33764cf115ce5dd067a1f0e2e
root <root@localground.org-2>
2017-02-14 00:13:09 +0000

    updates.  Trying to get things working in one try

--- deploy_tools/bootstrap.sh
+++ deploy_tools/bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -ex
 
+
+
 # if you are building a server as oposed to a vagrant box, pass 'server' as the first argument to this script.
 if [ "$1" == "server" ]; then
   SERVER=true
+  APP_INSTALL_DIR=/var/www/localground
 else
   SERVER=false
+  APP_INSTALL_DIR=/localground
 fi
 
 sudo apt-get update
@@ -59,13 +63,15 @@ echo "Y" | sudo apt-get install libpq-dev
 # Configure PostgreSQL / PostGIS #
 ##################################
 # Doing some automatic config file manipulations for postgres / postgis:
-DB_OWNER="postgres"
 if $SERVER; then
-  DB_NAME='lg'
+  DB_NAME='lg_prod'
+  DB_OWNER='localground'
+  DB_PASSWORD=$(openssl rand -base64 32)
 else
   DB_NAME="lg_test_database"
+  DB_OWNER="postgres"
+  DB_PASSWORD="password"
 fi
-DB_PASSWORD="password"
 PG_VERSION=9.3
 PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
 PEER="local   all             postgres                                peer"
@@ -78,7 +84,17 @@ CREATE_SQL="create database $DB_NAME;"
 psql -c "$CREATE_SQL" -U postgres
 psql -c "CREATE EXTENSION postgis;" -U postgres -d $DB_NAME
 psql -c "CREATE EXTENSION postgis_topology;" -U postgres -d $DB_NAME
-psql -c "alter user postgres with encrypted password '$DB_PASSWORD';" -U postgres
+
+if $SERVER; then
+  psql -c "CREATE USER $DB_OWNER WITH PASSWORD '$DB_PASSWORD';" -U postgres
+  psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_OWNER;" -U postgres
+  psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_OWNER;" -U postgres
+  psql -c "GRANT ALL ON ALL SEQUENCES IN SCHEMA public to $DB_OWNER;" -U postgres
+  psql -c "GRANT ALL ON ALL FUNCTIONS IN SCHEMA public to $DB_OWNER;" -U postgres
+else
+  psql -c "alter user $DB_OWNER with encrypted password '$DB_PASSWORD';" -U postgres
+fi
+
 sudo sed -i "s/$TRUST/$MD5/g" $PG_HBA
 sudo service postgresql restart
 
@@ -110,8 +126,18 @@ echo "Y" | sudo apt-get install redis-server
 # Install PIP Dependencies #
 ############################
 # there may be some problems with the map script / map server install
-sudo ln -s /vagrant /localground
-sudo pip install -r /vagrant/deploy_tools/requirements.txt
+if $SERVER; then
+#  sudo pip install -r ./requirements.txt
+#  sudo mkdir $APP_INSTALL_DIR
+#  adduser deployer  
+  sudo chown -R deployer:deployer $APP_INSTALL_DIR
+  cd $APP_INSTALL_DIR/..
+  sudo -u deployer git clone https://github.com/LocalGround/localground.git
+  sudo chown -R www-data:deployer $APP_INSTALL_DIR
+else
+  sudo ln -s /vagrant $APP_INSTALL_DIR
+  sudo pip install -r /vagrant/deploy_tools/requirements.txt
+fi
 
 #############################
 # Install Node.js and Bower #
@@ -126,21 +152,27 @@ sudo pip install -r /vagrant/deploy_tools/requirements.txt
 ####################################
 sudo a2enmod proxy_http
 sudo a2enmod ssl
-sudo cp /localground/deploy_tools/apache_vagrant_config /etc/apache2/sites-available/localground.conf
-sudo ln -s /etc/apache2/sites-available/localground.conf /etc/apache2/sites-enabled/localground.conf
-sudo cp /localground/deploy_tools/settings_local.py /localground/apps/.
+if $SERVER; then
+  sudo cp $APP_INSTALL_DIR/deploy_tools/apache_server_config /etc/apache2/sites-available/localground.conf
+  sudo ln -s /etc/apache2/sites-available/localground.conf /etc/apache2/sites-enabled/localground.conf
+  sudo cp $APP_INSTALL_DIR/deploy_tools/settings_local.py $APP_INSTALL_DIR/apps/.
+else
+  sudo cp /localground/deploy_tools/apache_vagrant_config /etc/apache2/sites-available/localground.conf
+  sudo ln -s /etc/apache2/sites-available/localground.conf /etc/apache2/sites-enabled/localground.conf
+  sudo cp /localground/deploy_tools/settings_local.py /localground/apps/.
+fi
 sudo rm /etc/apache2/sites-enabled/000-default.conf 
 sudo service apache2 restart
 
 ###############################
 # Create required directories #
 ###############################
-cd /localground
+cd $APP_INSTALL_DIR
 mkdir -p userdata/media
 mkdir -p userdata/prints
 mkdir -p userdata/deleted
 #Avoiding the issue w/serving django contrib static files vs. Apache's alias
-sudo cp -r /usr/local/lib/python2.7/dist-packages/swampdragon/static/swampdragon /localground/static/swampdragon
+sudo cp -r /usr/local/lib/python2.7/dist-packages/swampdragon/static/swampdragon $APP_INSTALL_DIR/static/swampdragon
 #################
 # Install Redis #
 #################
@@ -148,24 +180,28 @@ sudo apt-get -y install redis-server rabbitmq-server
 
 # we use supervisor to run our celery worker 
 sudo apt-get -y install supervisor
-sudo cp /localground/deploy_tools/celeryd.conf /etc/supervisor/conf.d/celeryd.conf
+sudo cp $APP_INSTALL_DIR/deploy_tools/celeryd.conf /etc/supervisor/conf.d/celeryd.conf
 sudo mkdir /var/log/celery
 
 # flower will monitor celery
-sudo cp /localground/deploy_tools/flower.conf /etc/supervisor/conf.d/flower.conf
+sudo cp $APP_INSTALL_DIR/deploy_tools/flower.conf /etc/supervisor/conf.d/flower.conf
 
 
 ###############################################
 # Create required Django tables and run tests #
 ###############################################
-cd /localground/apps
+cd $APP_INSTALL_DIR/apps
 sudo ln -s /usr/lib/libgdal.so.1.17.1 /usr/lib/libgdal.so.1.17.0
 python manage.py syncdb --noinput
 python manage.py test --verbosity=2
 sudo service supervisor restart
 sudo supervisorctl restart celery
 
-echo '------------------------------------'
-echo ' Server configured. Check it out at '
-echo ' http://localhost:7777              '
-echo '------------------------------------'
+if $SERVER; then
+  echo "db Username is $DB_OWNER with password '$DB_PASSWORD'"
+else
+  echo '------------------------------------'
+  echo ' Server configured. Check it out at '
+  echo ' http://localhost:7777              '
+  echo '------------------------------------'
+fi
