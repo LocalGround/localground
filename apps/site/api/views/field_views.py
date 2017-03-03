0a92e19610fba54fc4022dc2b8c2dcfcffcca8c5
Sarah <vanwars@gmail.com>
2017-03-03 11:56:43 -0800

    bugfix. need to write one more test for delete functionality

--- apps/site/api/views/field_views.py
+++ apps/site/api/views/field_views.py
@@ -12,7 +12,20 @@ class FieldMixin(object):
             return form
         except models.Form.DoesNotExist:
             raise Http404
-    
+        
+    def reorder_fields_and_set_display_field_if_needed(self, form):
+        ordering = 1
+        has_display_field = False
+        for field in form.fields:
+            if field.is_display_field:
+                has_display_field = True
+            field.ordering = ordering
+            field.save()
+            ordering += 1
+        if not has_display_field and len(form.fields) > 0:
+            form.fields[0].is_display_field = True
+            form.fields[0].save()
+
     def update_ordering_and_display_field(self, instance, form):
         ordering = instance.ordering
         new_order = 1
@@ -34,7 +47,7 @@ class FieldMixin(object):
                 field.save()
             new_order += 1
     
-    def validate_is_valid_col_alias(self, col_alias, form):
+    def validate_is_valid_col_alias(self, col_alias, form, pk=None):
         #if doesn't exist, no need to validate:
         if not col_alias:
             return
@@ -46,7 +59,7 @@ class FieldMixin(object):
             raise exceptions.ParseError('"%s" is a reserved column name' % col_alias)
         # ensure that it doesn't already exist:
         for f in form.fields:
-            if f.col_alias.lower() == col_alias:
+            if f.col_alias.lower() == col_alias and f.id != pk:
                 raise exceptions.ParseError(
                     'There is already a form field called "%s"' % col_alias)
     
@@ -92,7 +105,12 @@ class FieldInstance(FieldMixin, generics.RetrieveUpdateDestroyAPIView):
         # Todo: move functionality to Serializer
         form = self.get_form()
         data = serializer.validated_data
-        self.validate_is_valid_col_alias(data.get('col_alias'), form)
+        self.validate_is_valid_col_alias(data.get('col_alias'), form, pk=int(self.kwargs.get('pk')))
         self.validate_ordering_value(data.get('ordering'), form)
         instance = serializer.save()
         self.update_ordering_and_display_field(instance, form)
+  
+    def perform_destroy(self, instance):
+        form = instance.form
+        instance.delete()
+        self.reorder_fields_and_set_display_field_if_needed(form)
