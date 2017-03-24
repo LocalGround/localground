ef85a8a054331c49d948525ca7d0e76756c76db5
Sarah <vanwars@gmail.com>
2017-03-23 17:03:22 -0700

    added security to app

--- apps/site/urls.py
+++ apps/site/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import *
 from django.conf import settings
 from django.shortcuts import render as direct_to_template
+from django.contrib.auth.decorators import login_required
 import sys
 from django.contrib import admin
 
@@ -8,10 +9,10 @@ urlpatterns = patterns('',
 
     # mostly static html:
     (r'^$', direct_to_template, {'template_name': 'pages/splash.html'}),
-    (r'^map/$', direct_to_template, {'template_name': 'pages/map.html'}),
-    (r'^gallery/$', direct_to_template, {'template_name': 'pages/gallery.html'}),
-    (r'^table/$', direct_to_template, {'template_name': 'pages/spreadsheet.html'}),
-    (r'^style/$', direct_to_template, {'template_name': 'pages/style.html'}),
+    (r'^map/$', login_required(direct_to_template), {'template_name': 'pages/map.html'}),
+    (r'^gallery/$', login_required(direct_to_template), {'template_name': 'pages/gallery.html'}),
+    (r'^table/$', login_required(direct_to_template), {'template_name': 'pages/spreadsheet.html'}),
+    (r'^style/$', login_required(direct_to_template), {'template_name': 'pages/style.html'}),
     (r'^presentation/$', direct_to_template, {'template_name': 'pages/presentation.html'}),
     (r'^pages/(?P<page_name>\w+)/', 'localground.apps.site.views.pages.about_pages'),
 
