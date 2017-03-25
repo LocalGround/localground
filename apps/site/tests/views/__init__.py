f5ad199b38378756d3c6921587a8450fc6fdbad6
Sarah <vanwars@gmail.com>
2017-03-24 17:16:19 -0700

    added new tessts

--- apps/site/tests/views/__init__.py
+++ apps/site/tests/views/__init__.py
@@ -7,6 +7,7 @@ from localground.apps.site.tests.views.sharing_tests import *
 from localground.apps.site.tests.views.upload_tests import *
 '''
 from django import test
+from django.shortcuts import render
 from localground.apps.site.views import maps
 from localground.apps.site.tests import ViewMixin, ViewAnonymousMixin
 from rest_framework import status
@@ -16,10 +17,21 @@ class GenericTests(test.TestCase, ViewAnonymousMixin):
     def setUp(self):
         ViewAnonymousMixin.setUp(self)
         self.urls = [
-            '/map/', '/table/', '/gallery/', '/presentation/'
+            '/map/', '/table/', '/gallery/', '/presentation/', '/'
         ]
+        self.view = render
         
-    def test_anonymous_raises_404_on_protected_pages(self, urls=None):
+    def test_anonymous_raises_302_redirect_on_protected_pages(self, urls=None):
         for url in ['/map/', '/table/', '/gallery/']:
             response = self.client_anonymous.get(url)
-            self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+            self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+    
+    def test_logged_in_user_returns_200_on_protected_pages(self, urls=None):
+        for url in ['/map/', '/table/', '/gallery/']:
+            response = self.client_user.get(url)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_anonymous_shows_200_on_public_pages(self, urls=None):
+        for url in ['/', '/presentation/']:
+            response = self.client_anonymous.get(url)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
