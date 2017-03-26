d473fe9311ef4062582e21723494eb29cdd7dfd2
Sarah <vanwars@gmail.com>
2017-03-25 21:48:36 -0700

    Added map markers

--- static/redesign/apps/style/views/left/layer-list-child-view.js
+++ static/redesign/apps/style/views/left/layer-list-child-view.js
@@ -1,40 +1,83 @@
-define(["marionette",
+define(["jquery",
+        "marionette",
         "handlebars",
-        "collections/layers",
+        'lib/maps/marker-overlays',
         "text!../../templates/left/layer-item.html"
     ],
-    function (Marionette, Handlebars, Layers, LayerItemTemplate) {
+    function ($, Marionette, Handlebars, OverlayListView, LayerItemTemplate) {
         'use strict';
         var LayerListChild =  Marionette.ItemView.extend({
             initialize: function (opts) {
+                console.log('initialize childView');
                 _.extend(this, opts);
-            // this.listenTo(this.app.vent, 'update-title', this.updateTitle);
-            this.listenTo(this.model, "change", this.render);
+                // this.listenTo(this.app.vent, 'update-title', this.updateTitle);
+                this.listenTo(this.app.vent, "change-map", this.hide);
+                this.listenTo(this.model, "change", this.render);
+                this.initMapOverlays();
             },
             template: Handlebars.compile(LayerItemTemplate),
+            markerOverlayList: null,
             modelEvents: {},
             events: {
                 //edit event here, pass the this.model to the right panel
-                "click .edit" : "sendCollection"
-                },
-            tagName: "div",
-            className: "column",
-            templateHelpers: function () {
-                return {
-                    test: "123"
-                };
+                "click .edit" : "sendCollection",
+                'change input': 'showHide'
             },
 
-            sendCollection: function() {
+            sendCollection: function () {
                 this.app.vent.trigger("edit-layer", this.model);
-                console.log(this.model);
             },
-
 
             updateTitle: function (title) {
                 this.model.set("title", title);
-                console.log("should work");
                 this.render();
+            },
+
+            initMapOverlays: function () {
+                // create an OverlayListView for each symbol in the
+                // layer.
+                this.markerOverlayList = [];
+                var matchedCollection,
+                    overlays,
+                    that = this,
+                    dataSource = this.model.get("data_source"),
+                    data = this.app.dataManager.getCollection(dataSource);
+                _.each(this.model.getSymbols(), function (symbol) {
+                    matchedCollection = new data.constructor(null, { url: "dummy" });
+                    data.each(function (model) {
+                        if (symbol.checkModel(model)) {
+                            matchedCollection.add(model);
+                        }
+                    });
+                    overlays = new OverlayListView({
+                        collection: matchedCollection,
+                        app: that.app,
+                        iconOpts: symbol.toJSON(),
+                        isShowing: false
+                    });
+                    that.markerOverlayList.push(overlays);
+                });
+            },
+
+            show: function () {
+                _.each(this.markerOverlayList, function (overlays) {
+                    overlays.showAll();
+                });
+            },
+
+            hide: function () {
+                _.each(this.markerOverlayList, function (overlays) {
+                    overlays.hideAll();
+                });
+            },
+
+            showHide: function (e) {
+                var isChecked = $(e.target).prop('checked');
+                if (isChecked) {
+                    this.show();
+                } else {
+                    this.hide();
+                }
             }
         });
         return LayerListChild;
