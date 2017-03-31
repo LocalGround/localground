304eeb9172ab51a2be8c7dbd58e3fb54b76e387a
MrJBRPG <mrjbrpg@gmail.com>
2017-03-31 16:09:26 -0700

    Almost there with the radio buttons. Need more polishing

--- static/redesign/apps/gallery/views/field-child-view.js
+++ static/redesign/apps/gallery/views/field-child-view.js
@@ -9,12 +9,23 @@ define([
     var FieldChildView = Marionette.ItemView.extend({
         initialize: function (opts) {
             _.extend(this, opts);
+            this.getDisplayField();
         },
         events: {
             'click .delete-field': 'doDelete'
         },
         template: Handlebars.compile(FieldItemTemplate),
         tagName: "tr",
+        //*
+        getDisplayField: function(){
+            console.log(this.model.get("is_display_field"));
+            console.log(this.$el);
+            // I get context undefined each time I attempt to find class that holds the radio input
+            var $displayFieldRadio = this.$el.find(".display_field_button");
+            console.log($displayFieldRadio);
+            //$displayFieldRadio.
+        },
+        //*/
         doDelete: function (e) {
             if (!confirm("Are you sure you want to remove this field from the form?")) {
                 return;
@@ -26,6 +37,7 @@ define([
             this.model.destroy();
             e.preventDefault();
         }
+
     });
     return FieldChildView;
 
