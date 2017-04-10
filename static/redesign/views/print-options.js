0dc99b9000e40ca74729fcf7171d8dd45a36992b
MrJBRPG <mrjbrpg@gmail.com>
2017-04-10 13:59:29 -0700

    Set up rough draft of printoptions.js will work on more of it soon.

--- static/redesign/views/print-options.js
+++ static/redesign/views/print-options.js
@@ -1,2 +1,26 @@
-//
-//
+define(["jquery",
+        "marionette",
+        "handlebars",
+        "lib/modals/modal",
+        "lib/maps/basemap",
+        "text!../templates/print-layout.html"
+    ],
+    function ($, Marionette, Handlebars, Modal,
+              Basemap, PrintLayoutTemplate) {
+        'use strict';
+        // More info here: http://marionettejs.com/docs/v2.4.4/marionette.layoutview.html
+        var PrintOptions = Marionette.LayoutView.extend({
+            template: Handlebars.compile(PrintLayoutTemplate),
+
+            basemapView: null,
+
+            initialize: function (opts) {
+                /*This Layout View relies on a Map model which gets set from the change-map event,
+                which is triggered from the select-map-view.js */
+                this.app = opts.app;
+                this.render();
+                //this.showBasemap();
+            }
+        });
+        return PrintOptions;
+    });
