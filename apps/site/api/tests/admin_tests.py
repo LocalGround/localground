c990a8e4482bd69683ccb78ad562e4784a3d24fa
Sarah <vanwars@gmail.com>
2017-04-14 13:31:41 -0700

    tests fixed?

--- apps/site/api/tests/admin_tests.py
+++ apps/site/api/tests/admin_tests.py
@@ -1,4 +1,4 @@
-from localground.apps.site.api.tests.base_tests import ViewMixinAPISuperuser
+from localground.apps.site.api.tests.base_tests import ViewMixinAPISuperuser, ViewMixinAPI
 from django import test
 from localground.apps.site.api import views
 
@@ -35,17 +35,50 @@ class ApiDataTypeInstanceTest(test.TestCase, ViewMixinAPISuperuser):
         self.view = views.DataTypeViewSet.as_view({'get': 'detail'})
 
 
-class ApiTileListTest(test.TestCase, ViewMixinAPISuperuser):
+class ApiTileListTest(test.TestCase, ViewMixinAPI):
 
     def setUp(self):
-        ViewMixinAPISuperuser.setUp(self)
+        ViewMixinAPI.setUp(self)
         self.urls = ['/api/0/tiles/']
         self.view = views.TileSetList.as_view()
+        self.metadata = {
+            'id': { 'type': 'integer', 'required': False, 'read_only': True  },
+            'url': { 'type': 'field', 'required': False, 'read_only': True  },
+            'name': { 'type': 'string', 'required': False, 'read_only': False  },
+            'tags': { 'type': 'field', 'required': False, 'read_only': False  },
+            'overlay_source': { 'type': 'field', 'required': True, 'read_only': False },
+            'base_tile_url': { 'type': 'string', 'required': False, 'read_only': False },
+            'base_static_url': { 'type': 'string', 'required': False, 'read_only': False },
+            'source_name': { 'type': 'field', 'required': False, 'read_only': True },
+            'max_zoom': { 'type': 'integer', 'required': False, 'read_only': False },
+            'min_zoom': { 'type': 'integer', 'required': False, 'read_only': False },
+            'is_printable': { 'type': 'boolean', 'required': False, 'read_only': False },
+            'extras': { 'type': 'json', 'required': False, 'read_only': False },
+            'key': { 'type': 'string', 'required': False, 'read_only': False },
+            'owner': { 'type': 'field', 'required': False, 'read_only': True },
+            'attribution': { 'type': 'string', 'required': False, 'read_only': False }
+        }
 
-
-class ApiTileInstanceTest(test.TestCase, ViewMixinAPISuperuser):
+class ApiTileInstanceTest(test.TestCase, ViewMixinAPI):
 
     def setUp(self):
-        ViewMixinAPISuperuser.setUp(self)
+        ViewMixinAPI.setUp(self)
         self.urls = ['/api/0/tiles/1/', '/api/0/tiles/2/', '/api/0/tiles/3/']
         self.view = views.TileSetInstance.as_view()
+        self.metadata = {
+            'id': { 'type': 'integer', 'required': False, 'read_only': True  },
+            'url': { 'type': 'field', 'required': False, 'read_only': True  },
+            'name': { 'type': 'string', 'required': False, 'read_only': False  },
+            'tags': { 'type': 'field', 'required': False, 'read_only': False  },
+            'overlay_source': { 'type': 'field', 'required': True, 'read_only': False },
+            'base_tile_url': { 'type': 'string', 'required': False, 'read_only': False },
+            'base_static_url': { 'type': 'string', 'required': False, 'read_only': False },
+            'source_name': { 'type': 'field', 'required': False, 'read_only': True },
+            'max_zoom': { 'type': 'integer', 'required': False, 'read_only': False },
+            'min_zoom': { 'type': 'integer', 'required': False, 'read_only': False },
+            'is_printable': { 'type': 'boolean', 'required': False, 'read_only': False },
+            'extras': { 'type': 'json', 'required': False, 'read_only': False },
+            'key': { 'type': 'string', 'required': False, 'read_only': False },
+            'owner': { 'type': 'field', 'required': False, 'read_only': True },
+            'attribution': { 'type': 'string', 'required': False, 'read_only': False }
+        }
