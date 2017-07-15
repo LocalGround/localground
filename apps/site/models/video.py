ee5053c5596e6ab0bfeab01eb326b837746b8a50
Sarah <vanwars@gmail.com>
2017-07-15 10:47:28 -0700

    organized media browser and added video capacity

--- apps/site/models/video.py
+++ apps/site/models/video.py
@@ -14,26 +14,26 @@ class Video(ProjectMixin, BaseAudit, BasePointMixin, models.Model):
     description = models.TextField(null=True, blank=True, verbose_name="caption")
     tags = ArrayField(models.TextField(), default=list)
     video_id = models.CharField(null=False, blank=False, max_length=255)
-    provider = models.CharField(max_length=63, null=False, blank=False, choices=VIDEO_PROVIDERS, verbose_name="video provider")
-
+    provider = models.CharField(max_length=63, null=False, blank=False,
+                    choices=VIDEO_PROVIDERS, verbose_name="video provider")
+    '''
+    # TODO: We forgot to add attribution:
+    attribution = models.CharField(max_length=500, blank=True,
+                                   null=True, verbose_name="Author / Creator",
+                                   help_text="Name of the person / group who created the media file (text)")
+    '''
+    
+    filter_fields = filter_fields = ('id', 'project', 'date_created', 'name', 'description', 'tags', 'point')
     objects = VideoManager()
-
+    
     def __str__(self):
-        return self.name
+        return '{0}: {1} ({2}: {3})'.format(self.id, self.name, self.video_id, self.provider)
 
     def __repr__(self):
-        return self.name
+        return '{0}: {1} ({2}: {3})'.format(self.id, self.name, self.video_id, self.provider)
 
     def can_view(self, user, access_key=None):
-        '''raise Exception(user)
-        raise Exception(self.project.id)'''
         return self.project.can_view(user=user, access_key=access_key)
 
     def can_edit(self, user):
         return self.project.can_edit(user)
-
-    class Meta:
-        app_label = 'site'
-        ordering = ['id']
-        verbose_name = 'video'
-        verbose_name_plural = 'videos'
--- static/redesign/apps/gallery/views/media-browser-uploader.js
+++ static/redesign/apps/gallery/views/media-browser-uploader.js
@@ -5,8 +5,8 @@ define([
 
     var MedaBrowserUploader = CreateMedia.extend({
         initialize: function (opts) {
-            CreateMedia.prototype.initialize.call(this);
-       } 
+            CreateMedia.prototype.initialize.call(this, opts);
+        }
     });
     return MedaBrowserUploader;
 
