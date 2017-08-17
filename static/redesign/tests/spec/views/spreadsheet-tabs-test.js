fdf2a6eafbc05cc6749d534b87669bca9d82d492
MrJBRPG <mrjbrpg@gmail.com>
2017-08-17 13:52:01 -0700

    Completed the spreadsheet tab test

--- static/redesign/tests/spec/views/spreadsheet-tabs-test.js
+++ static/redesign/tests/spec/views/spreadsheet-tabs-test.js
@@ -14,6 +14,8 @@ function(Handlebars, Spreadsheet, SpreadsheetTabs, Form){
     var setupSpreadsheetTabTest = function (scope) {
 
         spyOn(scope.app.vent, 'trigger').and.callThrough();
+        spyOn(SpreadsheetTabs.prototype, "switchTab").and.callThrough();
+        spyOn(SpreadsheetTabs.prototype, "render").and.callThrough();
         newSpreadsheetTabs = new SpreadsheetTabs({
             app: scope.app,
             forms: scope.forms
@@ -22,7 +24,7 @@ function(Handlebars, Spreadsheet, SpreadsheetTabs, Form){
         fixture = setFixtures("<div></div>").append(newSpreadsheetTabs.$el);
     };
 
-    describe("Spreadsheet Tabs: Render", function () {
+    describe("Spreadsheet Tabs Test", function () {
         beforeEach(function () {
             setupSpreadsheetTabTest(this);
         });
@@ -30,7 +32,7 @@ function(Handlebars, Spreadsheet, SpreadsheetTabs, Form){
          * 1. Check to make sure that there's a tab for photos, audio, sites, videos, and any form
          * in the forms collection.
          *
-         * 2. Make sure that the tab name, class, and link are rendered correctly 
+         * 2. Make sure that the tab name, class, and link are rendered correctly
          *
          * 3. Make sure that when the tab is clicked, it triggers switchTab
          *
@@ -42,10 +44,45 @@ function(Handlebars, Spreadsheet, SpreadsheetTabs, Form){
          *
          */
 
-        it("Successfully renders as intended", function(){
-            // Rough draft setup until functions are well defined
-            console.log(fixture.html());
-            expect(1).toEqual(0);
+        it("Renders all the elements correctly", function(){
+
+            var tabs = fixture.find(".tab");
+            expect(tabs[0]).toHaveAttr("data-value", "photos");
+            expect(tabs[1]).toHaveAttr("data-value", "audio");
+            expect(tabs[2]).toHaveAttr("data-value", "videos");
+            expect(tabs[3]).toHaveAttr("data-value", "markers");
+            expect(tabs[4]).toHaveAttr("data-value", "form_1");
+            expect(tabs[5]).toHaveAttr("data-value", "form_2");
+            expect(tabs[0].innerHTML).toEqual("Photos");
+            expect(tabs[1].innerHTML).toEqual("Audio");
+            expect(tabs[2].innerHTML).toEqual("Videos");
+            expect(tabs[3].innerHTML).toEqual("Sites");
+            expect(tabs[4].innerHTML).toEqual("Test Form");
+            expect(tabs[5].innerHTML).toEqual("Animals");
+            expect(tabs[0]).toHaveAttr("href", "#/photos");
+            expect(tabs[1]).toHaveAttr("href", "#/audio");
+            expect(tabs[2]).toHaveAttr("href", "#/videos");
+            expect(tabs[3]).toHaveAttr("href", "#/markers");
+            expect(tabs[4]).toHaveAttr("href", "#/form_1");
+            expect(tabs[5]).toHaveAttr("href", "#/form_2");
+            expect(fixture.find(".tab-selected").length).toEqual(1);
         });
+
+        it("Successfully switches active tab after clicking on another", function(){
+            var tabs = fixture.find(".tab");
+            var activeTabOnStart = fixture.find(".tab-selected");
+            expect(this.app.dataType).toEqual("markers");
+            expect(SpreadsheetTabs.prototype.switchTab).toHaveBeenCalledTimes(0);
+            expect(SpreadsheetTabs.prototype.render).toHaveBeenCalledTimes(1);
+            expect(tabs[3]).toHaveClass("tab-selected");
+            $(tabs[0]).trigger('click');
+            tabs = fixture.find(".tab");
+            expect(SpreadsheetTabs.prototype.switchTab).toHaveBeenCalledTimes(1);
+            expect(SpreadsheetTabs.prototype.render).toHaveBeenCalledTimes(2);
+            expect(this.app.dataType).toEqual("photos");
+            expect(tabs[3]).not.toHaveClass("tab-selected");
+
+        });
+
     });
 });
