21b83696b59abf5cc0168bf1fad7498e01efebc3
Sarah <vanwars@gmail.com>
2017-08-25 11:46:52 -0700

    debugging...TODO: continue debugging later

--- static/redesign/apps/gallery/views/data-detail.js
+++ static/redesign/apps/gallery/views/data-detail.js
@@ -70,22 +70,24 @@ define([
         },
         featuredImageID: null,
         initialize: function (opts) {
+            $(window).scrollTop(0);
+            console.log("NNNNEEEEEWWWWW")
             this.mobileView = null;
             _.extend(this, opts);
             this.bindFields();
             this.dataType = this.dataType || this.app.dataType;
             Marionette.ItemView.prototype.initialize.call(this);
             $('#marker-detail-panel').addClass('mobile-minimize');
-            $(window).on("resize", _.bind(this.screenSize, this));
+            //$(window).on("resize", _.bind(this.screenSize, this));
            // $(window).scroll(this.detectScroll);
-            $(window).on("scroll",  _.bind(this.detectScroll, this));
+            //$(window).on("scroll",  _.bind(this.detectScroll, this));
            // $(document).on('scrollstart', _.bind(this.detectScroll, this));
             //$(window).on("scroll",  _.bind(this.detectScroll, this))
             this.isMobile();
 
 
             this.listenTo(this.app.vent, 'save-model', this.saveModel);
-            this.listenTo(this.app.vent, 'streetview-hidden',           this.updateStreetViewButton);
+            this.listenTo(this.app.vent, 'streetview-hidden', this.updateStreetViewButton);
         },
 
         expandMobile: function () {
@@ -114,11 +116,11 @@ define([
             console.log(oldTranslateY, this.getComputedTranslateY(dragEl), translateY);
             */
             console.log(scrollHeight, triggerHeight, scrollHeight < triggerHeight, this.mobileView !== "expanded", "this.mobileview = " + this.mobileView );
-            if (scrollHeight < triggerHeight && this.mobileView !== "expanded") {
+            /*if (scrollHeight < triggerHeight && this.mobileView !== "expanded") {
                 console.log("expand!");
                 this.expandMobile();
                // $(window).scrollTop(screen.height/2.5);
-            }
+           }*/
            // if (this.$el.find(".parallax."))
         },
 
@@ -133,27 +135,38 @@ define([
         },
 
         initParallax: function () {
+            console.log("initParallax");
             var that = this;
             var MoveItItem = function (el) {
                 this.initialPosition = $(el).position().top;
-                this.calculateDimensions = function () {
+
+                this.initPosition = function () {
                     this.targetTop = this.$el.attr('data-target-top').replace("%", "");
                     this.finalPosition = parseFloat(this.targetTop, 10) * $(window).height() / 100;
                     that.distance = this.initialPosition - this.finalPosition;
                     this.scrollDistance = Math.abs($(window).height() - $(document).height());
                     this.speed = that.distance / this.scrollDistance;
+                    this.className = this.$el.get(0).className;
+                    this.lastDirection = "up";
+                    this.lastScrollTop = 0;
+                     console.log("--------------------");
+                     console.log("className", this.className);
+                     console.log("initialPosition", this.initialPosition);
+                     console.log("finalPosition", this.finalPosition);
+                     console.log("scrollDistance", this.scrollDistance);
+                     console.log("distance", that.distance);
+                     console.log("speed", this.speed);
                 };
                 this.$el = $(el).css({
                     position: "fixed",
                     top: this.initialPosition
                 });
 
-                this.calculateDimensions();
-
-                this.lastDirection = "up";
-                this.lastScrollTop = 0;
+                this.initPosition();
 
                 this.update = function (scrollTop) {
+                    //console.log(this.className, scrollTop);
+                    //this.calculateDimensions();
                     var direction = (scrollTop > this.lastScrollTop) ? "up": "down";
                     if (direction !== this.lastDirection) {
                         console.log('switch');
@@ -161,7 +174,7 @@ define([
                     if (direction == "down" && scrollTop <= 50) {
                         that.$el.find('.expanded').hide();
                         that.$el.find('.contracted').show();
-                        /*this.calculateDimensions();
+                        /*this.initPosition();
                         console.log(that.distance);*/
                         console.log("showSmallTemplate", that.distance);
                     }
@@ -169,7 +182,7 @@ define([
                     if (direction == "up" && scrollTop >= 200) {
                         that.$el.find('.expanded').show();
                         that.$el.find('.contracted').hide();
-                        //this.calculateDimensions();
+                        //this.initPosition();
                         console.log("showBigTemplate", that.distance);
                     }
                     this.$el.css('top', this.initialPosition - scrollTop * this.speed);
@@ -180,22 +193,31 @@ define([
                 };
             };
             $.fn.moveIt = function () {
+                if (that.scrollEventListener) {
+                    console.log('removing...');
+                    window.removeEventListener("scroll", that.scrollEventListener);
+                }
                 var $window = $(window),
-                    instances = [];
+                    instances = [],
+                    moveItem;
+                console.log('THIS', $(this));
                 $(this).each(function () {
-                    instances.push(new MoveItItem($(this)));
+                    console.log('looping through selector...');
+                    moveItem = new MoveItItem($(this));
+                    moveItem.initPosition();
+                    instances.push(moveItem);
                 });
-
-                window.onscroll = function () {
+                console.log("initializing...");
+                that.scrollEventListener = function () {
                     var scrollTop = $window.scrollTop();
                     instances.forEach(function (inst) {
                         inst.update(scrollTop);
                     });
                 };
+                window.addEventListener("scroll", that.scrollEventListener);
             };
             $(function () {
-                $(window).scrollTop(0);
-                $('.parallax').moveIt();
+                that.$el.find('.parallax').moveIt();
             });
         },
 
@@ -542,6 +564,7 @@ define([
             // setTimeout necessary to register DOM element
             //setTimeout(this.initDraggable.bind(this), 50);
             if ($(window).width() < 900) {
+                this.isMobile();
                 this.initParallax();
             }
         },
