9ddbab611438011f7ca7b2513d229795eb51747c
Sarah <vanwars@gmail.com>
2017-09-21 18:02:26 -0700

    debugging router

--- static/apps/style/controller.js
+++ static/apps/style/controller.js
@@ -20,15 +20,21 @@ define([
             var that = this;
             if (this.deferredLayerID) {
                 setTimeout(function () {
-                    that.app.router.navigate("//" + that.deferredMapID + "/layers/" + that.deferredLayerID, {trigger: true});
+                    that.app.vent.trigger('route-layer', that.deferredMapID, that.deferredLayerID);
                     delete that.deferredMapID;
                     delete that.deferredLayerID;
                 }, 400);
                 return;
-            }
-            setTimeout(function () {
-                that.app.router.navigate("//new", {trigger: true});
-            }, 400);
+            } else if (this.deferredMapID) {
+                setTimeout(function () {
+                    that.app.vent.trigger('route-new-layer');
+                    delete that.deferredMapID;
+                }, 400);
+            }/* else {
+                setTimeout(function () {
+                    that.app.vent.trigger('route-new-map');
+                }, 400);
+            }*/
         },
         displayMap: function (mapId) {
             console.log("display map");
@@ -50,8 +56,13 @@ define([
             }
             this.app.vent.trigger('route-new-map');
         },
-        newLayer: function() {
+
+        newLayer: function(mapID) {
             console.log("newLayer");
+            if (!this.isDataLoaded()) {
+                this.stashParams(mapID);
+                return
+            }
             this.app.vent.trigger('route-new-layer');
         },
 
