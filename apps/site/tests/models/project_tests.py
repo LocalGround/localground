0312d83666283e9ac5f6886f4a66b16faa918619
Sarah <vanwars@gmail.com>
2017-10-20 10:58:32 -0700

    WIP: project_tests

---
+++ apps/site/tests/models/project_tests.py
@@ -0,0 +1,18 @@
+from localground.apps.site import models
+from localground.apps.site.tests.models.abstract_base_uploaded_media_tests \
+    import BaseUploadedMediaAbstractModelClassTest
+
+
+class ProjectModelTest(NamedMixin, GenericRelationMixin,
+                       ObjectPermissionsMixin, BaseAudit):
+
+    def setUp(self):
+        BaseUploadedMediaAbstractModelClassTest.setUp(self)
+
+    def tearDown(self):
+        # delete method also removes files from file system:
+        for audio in models.Audio.objects.all():
+            audio.delete()
+
+    def test_dummy_audio(self, **kwargs):
+        self.assertEqual(1, 1)
