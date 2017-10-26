ccae8c7b1fdce4a3aab168309f38d2508d3c8f5a
MrJBRPG <mrjbrpg@gmail.com>
2017-10-26 12:54:56 -0700

    Making some progress with the form test, but expectedly got stuck because I cannot process the necessary information through print alone in hopes of debugging

--- apps/site/tests/models/form_tests.py
+++ apps/site/tests/models/form_tests.py
@@ -15,5 +15,38 @@ class FormTest(BaseAbstractModelClassTest, test.TestCase):
         self.model = self.create_form()
 
     # Still need more to test...
-    def test_dummy(self, **kwargs):
+
+    def get_filter_fields_test(self, **kwargs):
+        # Looks like somehow it never reaches this assert
+        # from attempting to retrieve query fields data
+        query_fields_data = self.model.get_filter_fields()
+        print(query_fields_data)
+        self.assertEqual(1,0)
+
+    def cache_dynamic_models_test(self, **kwargs):
+        pass
+
+    def clear_table_model_cache_test(self, **kwargs):
+        pass
+
+    def get_fields_test(self, **kwargs):
+        # does not seem to reach self assert either
+        formWithFields = self.create_form_with_fields()
+        retrievedFields = formWithFields.get_fields()
+        print(retrievedFields)
         self.assertEqual(1, 0)
+
+    def get_model_class_test(self, **kwargs):
+        pass
+
+    def source_table_exists_test(self, **kwargs):
+        pass
+
+    def save_test(self, **kwargs):
+        pass
+
+    def delete_test(self, **kwargs):
+        pass
+
+    def remove_table_from_cache_test(self, **kwargs):
+        pass
