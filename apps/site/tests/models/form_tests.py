ae82042412f1579e466bdbdb377321705731cb12
Sarah <vanwars@gmail.com>
2017-10-26 13:24:31 -0700

    tests working and set up

--- apps/site/tests/models/form_tests.py
+++ apps/site/tests/models/form_tests.py
@@ -1,34 +1,29 @@
 from localground.apps.site.models import Form
-
 from django.contrib.gis.db import models
-
-from localground.apps.site.tests.models.abstract_bases import \
+from localground.apps.site.tests.models.abstract_base_tests import \
     BaseAbstractModelClassTest
 from django import test
 
 
 # form test in progress
-class FormTest(test.TestCase):
+class FormTest(BaseAbstractModelClassTest, test.TestCase):
 
-    #def setUp(self):
-    #    BaseAbstractModelClassTest.setUp(self)
-        #self.model = self.create_form()
-
-    # Still need more to test...
+    def setUp(self):
+        BaseAbstractModelClassTest.setUp(self)
+        self.model = self.create_form()
 
     def test_get_filter_fields(self, **kwargs):
         # Looks like somehow it never reaches this assert
         # from attempting to retrieve query fields data
-        #query_fields_data = self.model.get_filter_fields()
-        #print(query_fields_data)
-        self.assertEqual(1,0)
+        query_fields_data = self.model.get_filter_fields()
+        print(query_fields_data)
+        self.assertEqual(1, 0)
 
-'''
     def test_cache_dynamic_models(self, **kwargs):
-        pass
+        self.assertEqual(1, 0)
 
     def test_clear_table_model_cache(self, **kwargs):
-        pass
+        self.assertEqual(1, 0)
 
     def test_get_fields(self, **kwargs):
         # does not seem to reach self assert either
@@ -38,17 +33,16 @@ class FormTest(test.TestCase):
         self.assertEqual(1, 0)
 
     def test_get_model_class(self, **kwargs):
-        pass
+        self.assertEqual(1, 0)
 
     def test_source_table_exists(self, **kwargs):
-        pass
+        self.assertEqual(1, 0)
 
     def test_save(self, **kwargs):
-        pass
+        self.assertEqual(1, 0)
 
     def test_delete(self, **kwargs):
-        pass
+        self.assertEqual(1, 0)
 
     def test_remove_table_from_cache(self, **kwargs):
-        pass
-'''
+        self.assertEqual(1, 0)
