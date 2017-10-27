95c26057aa0ce55e275aa2ac2665636f3557e002
MrJBRPG <mrjbrpg@gmail.com>
2017-10-27 16:27:57 -0700

    minor edits

--- apps/site/tests/models/mapimage_tests.py
+++ apps/site/tests/models/mapimage_tests.py
@@ -12,25 +12,24 @@ class MapImageTest(BaseAbstractModelClassTest, test.TestCase):
         BaseAbstractModelClassTest.setUp(self)
         self.model = MapImage()
 
-
     # A streamlined approach to checking all the properties
     # from the class being tested on
     def test_model_properties(self, **kwargs):
         for prop in [
-            ('uuid' ,models.CharField),
-            ('source_print' , models.ForeignKey),
-            ('status' , models.ForeignKey),
-            ('file_name_thumb' , models.CharField),
-            ('file_name_scaled' , models.CharField),
-            ('scale_factor' , models.FloatField),
-            ('upload_source' , models.ForeignKey),
-            ('email_sender' , models.CharField),
-            ('email_subject' , models.CharField),
-            ('email_body' , models.TextField),
-            ('qr_rect' , models.CharField),
-            ('qr_code' , models.CharField),
-            ('map_rect' , models.CharField),
-            ('processed_image' , models.ForeignKey)
+            ('uuid', models.CharField),
+            ('source_print', models.ForeignKey),
+            ('status', models.ForeignKey),
+            ('file_name_thumb', models.CharField),
+            ('file_name_scaled', models.CharField),
+            ('scale_factor', models.FloatField),
+            ('upload_source', models.ForeignKey),
+            ('email_sender', models.CharField),
+            ('email_subject', models.CharField),
+            ('email_body', models.TextField),
+            ('qr_rect', models.CharField),
+            ('qr_code', models.CharField),
+            ('map_rect', models.CharField),
+            ('processed_image', models.ForeignKey)
         ]:
             prop_name = prop[0]
             prop_type = prop[1]
