7308bf38d805df8e7b75049d66098f87ce9778b2
MrJBRPG <mrjbrpg@gmail.com>
2017-11-17 16:18:45 -0800

    minor edits

---
+++ apps/site/migrations/0038_audio_uploaded_file.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('site', '0037_document'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='audio',
+            name='uploaded_file',
+            field=models.FileField(null=True, upload_to=b''),
+        ),
+    ]
