870f8c50eb0be674895b23ada8cf0edc6b75c7f9
MrJBRPG <mrjbrpg@gmail.com>
2017-11-17 16:01:12 -0800

    leaving some thought process on file upload to AWS

--- apps/site/models/audio.py
+++ apps/site/models/audio.py
@@ -4,21 +4,41 @@ from localground.apps.site.models import ExtrasMixin
 from localground.apps.site.models import BaseUploadedMedia
 from localground.apps.lib.helpers import upload_helpers
 import os
+from django.db import models
 
+'''
+Here is the pattern that has to be broken down for a valid url for AWS S3:
+
+example:
+https://s3test-assets.s3.amazonaws.com/media/Screen_Shot_2017-11-17_at_12.14.59_PM.png
+
+breakdown of the http address:
+https:// {bucketname} .s3.amazonaws.com / {path to media file} / {media file}
+
+somehow, the uploaded file has to be connected with the media
+at the end of the url.
+
+Furthermore, when the file is finished processing,
+it will be appropriate to assign the uploaded_file
+to the one that is tied to a Document object
+
+i.e. doc.upload.url [use-url from FileField] & doc.upload.file
+'''
 
 class Audio(ExtrasMixin, PointMixin, BaseUploadedMedia):
+    uploaded_file = models.FileField(null=True)
     objects = AudioManager()
 
     @classmethod
     def process_file(cls, file, owner, name=None):
-        
+
         #save to disk:
         model_name_plural = cls.model_name_plural
         file_name_new = upload_helpers.save_file_to_disk(
             owner, model_name_plural, file
         )
         file_name, ext = os.path.splitext(file_name_new)
-        
+
         # convert to MP3:
         if ext != '.mp3':
             # use ffmpeg to convert to mp3:
@@ -31,7 +51,7 @@ class Audio(ExtrasMixin, PointMixin, BaseUploadedMedia):
             command = 'ffmpeg -loglevel panic -i \'%s\' -ab 32k -ar 22050 -y \'%s\'' % \
                 (path_to_be_converted, path_to_mp3)
             result = os.popen(command)
-        
+
         return {
             'file_name_orig': file.name,
             'name': name or file.name,
