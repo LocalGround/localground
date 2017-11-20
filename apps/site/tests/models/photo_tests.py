f726fb43ea116e862a04e5a2af3c37456d007b3c
Sarah <vanwars@gmail.com>
2017-11-20 15:22:31 -0800

    code review and removing comments

--- apps/site/tests/models/photo_tests.py
+++ apps/site/tests/models/photo_tests.py
@@ -16,8 +16,6 @@ from localground.apps.site import models
 from localground.apps.lib.helpers import get_timestamp_no_milliseconds
 from django.core.files import File
 import json
-
-
 import urllib
 
 
@@ -38,14 +36,15 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
         # delete method also removes files from file system:
         for photo in models.Photo.objects.all():
             photo.remove_media_from_file_system()
-    
+
     def test_photo_file_thumbnail_generator_works(self, **kwargs):
         """
         Step 1: move process_file() from photo serializer to photo model
-        Step 2: instead of 'with open' post to api, 
+        Step 2: instead of 'with open' post to api,
             do 'with open' and directly call model.process_file()
             (don't go through the api)
-        Step 3: refactor more to make photo model DRY. Dont repeat code in rotate()
+        Step 3: refactor more to make photo model DRY. Dont repeat code in
+        rotate()
         """
 
         user = self.user
@@ -55,21 +54,8 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
         image.save(tmp_file)
         author_string = 'Author of the media file'
         tags = "j,k,l"
-        
+
         with open(tmp_file, 'rb') as data:
-    
-            '''
-            response = self.client_user.post(
-                '/api/0/photos/',
-                {
-                    'project_id': project.id,
-                    'media_file': data,
-                    'attribution': author_string,
-                    'tags': tags
-                },
-                HTTP_X_CSRFTOKEN=self.csrf_token
-            )
-            '''
             photo_data = {
                 'attribution': user.username,
                 'host': settings.SERVER_HOST,
@@ -82,12 +68,12 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
                 File(data), user
             )
             photo_data.update(attributes_from_processed_file)
-            #photo = models.Photo.objects.get(id=response.get("id"))
-            #result.update(self.get_presave_create_dictionary())
-            
+            # photo = models.Photo.objects.get(id=response.get("id"))
+            # result.update(self.get_presave_create_dictionary())
+
             photo = models.Photo.objects.create(**photo_data)
             media_path = photo.get_absolute_path()
-            
+
             for file_name in [
                 photo.file_name_large,
                 photo.file_name_medium,
@@ -99,7 +85,7 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
                 self.assertTrue(len(file_name) > 4)
                 self.assertTrue(os.path.exists(
                     '%s%s' % (media_path, file_name)))
-            
+
             return photo
 
     def test_photo_rotates_right(self, **kwargs):
@@ -166,7 +152,6 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
         for path in new_paths:
             self.assertTrue(os.path.exists(path))
 
-    
     def test_model_properties(self, **kwargs):
         from django.contrib.gis.db import models
         from localground.apps.site.models import BaseUploadedMedia
@@ -184,20 +169,20 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
             field = Photo._meta.get_field(prop_name)
             self.assertEqual(field.name, prop_name)
             self.assertEqual(type(field), prop_type)
-        
+
         self.assertTrue(hasattr(self.model, 'filter_fields'))
 
     def test_thumb(self, **kwargs):
         thumb_url = self.model.encrypt_url(self.model.file_name_small)
         self.assertEqual(self.model.thumb(), thumb_url)
-        
+
     def test_absolute_virtual_path_medium_sm(self):
         path_md_sm = self.model.encrypt_url(self.model.file_name_medium_sm)
         self.assertEqual(
-            self.model.absolute_virtual_path_medium_sm(), 
+            self.model.absolute_virtual_path_medium_sm(),
             path_md_sm
         )
-    
+
     def test_absolute_virtual_path_medium(self):
         path_medium = self.model.encrypt_url(self.model.file_name_medium)
         self.assertEqual(
@@ -211,10 +196,10 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
             self.model.absolute_virtual_path_large(),
             path_large
         )
-    
+
     def test_remove_media_from_file_system(self):
         import os
-        path = self.model.get_absolute_path();
+        path = self.model.get_absolute_path()
         file_paths = [
             self.model.file_name_orig,
             self.model.file_name_new,
@@ -225,16 +210,15 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
             self.model.file_name_marker_lg,
             self.model.file_name_marker_sm
         ]
-        
 
         photo = self.test_photo_file_thumbnail_generator_works()
-        
+
         photo.remove_media_from_file_system()
         self.assertFalse(os.path.exists(
             '%s%s' % (path, photo.file_name_large)
             )
         )
-    
+
     def test_unicode_(self):
         test_string = '%s (%s)' % (
             self.model.name, self.model.file_name_orig
@@ -244,7 +228,7 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
     def test_read_exif_data(self):
         import io
         from PIL import Image
-        from PIL.ExifTags import TAGS, GPSTAGS 
+        from PIL.ExifTags import TAGS, GPSTAGS
         import piexif
 
         image = Image.new('RGB', (200, 100))
@@ -257,39 +241,52 @@ class PhotoModelTest(ExtrasMixinTest, PointMixinTest, ProjectMixinTest,
         thumb_im.save(o, "jpeg")
         thumbnail = o.getvalue()
 
-        zeroth_ifd = {piexif.ImageIFD.Make: u"Canon",
-                    piexif.ImageIFD.XResolution: (96, 1),
-                    piexif.ImageIFD.YResolution: (96, 1),
-                    piexif.ImageIFD.Software: u"piexif"
-                    }
-        exif_ifd = {piexif.ExifIFD.DateTimeOriginal: u"2099:09:29 10:10:10",
-                    piexif.ExifIFD.LensMake: u"LensMake",
-                    piexif.ExifIFD.Sharpness: 65535,
-                    piexif.ExifIFD.LensSpecification: ((1, 1), (1, 1), (1, 1), (1, 1)),
-                    }
-        gps_ifd = {piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
-                piexif.GPSIFD.GPSAltitudeRef: 1,
-                piexif.GPSIFD.GPSDateStamp: u"1999:99:99 99:99:99",
-                piexif.GPSIFD.GPSLatitude: ((122, 1), (16, 1), (2163, 100)),
-                piexif.GPSIFD.GPSLatitudeRef: "N",
-                piexif.GPSIFD.GPSLongitude: ((122, 1), (16, 1), (2163, 100)),
-                piexif.GPSIFD.GPSLongitudeRef: "W"
-                }
-        first_ifd = {piexif.ImageIFD.Make: u"Canon",
-                    piexif.ImageIFD.XResolution: (40, 1),
-                    piexif.ImageIFD.YResolution: (40, 1),
-                    piexif.ImageIFD.Software: u"piexif"
-                    }
+        zeroth_ifd = {
+            piexif.ImageIFD.Make: u"Canon",
+            piexif.ImageIFD.XResolution: (96, 1),
+            piexif.ImageIFD.YResolution: (96, 1),
+            piexif.ImageIFD.Software: u"piexif"
+        }
+        exif_ifd = {
+            piexif.ExifIFD.DateTimeOriginal: u"2099:09:29 10:10:10",
+            piexif.ExifIFD.LensMake: u"LensMake",
+            piexif.ExifIFD.Sharpness: 65535,
+            piexif.ExifIFD.LensSpecification: ((1, 1), (1, 1), (1, 1), (1, 1)),
+        }
+        gps_ifd = {
+            piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
+            piexif.GPSIFD.GPSAltitudeRef: 1,
+            piexif.GPSIFD.GPSDateStamp: u"1999:99:99 99:99:99",
+            piexif.GPSIFD.GPSLatitude: ((122, 1), (16, 1), (2163, 100)),
+            piexif.GPSIFD.GPSLatitudeRef: "N",
+            piexif.GPSIFD.GPSLongitude: ((122, 1), (16, 1), (2163, 100)),
+            piexif.GPSIFD.GPSLongitudeRef: "W"
+        }
+        first_ifd = {
+            piexif.ImageIFD.Make: u"Canon",
+            piexif.ImageIFD.XResolution: (40, 1),
+            piexif.ImageIFD.YResolution: (40, 1),
+            piexif.ImageIFD.Software: u"piexif"
+        }
 
-        exif_dict = {"0th":zeroth_ifd, "Exif":exif_ifd, "GPS":gps_ifd, "1st":first_ifd, "thumbnail":thumbnail}
+        exif_dict = {
+            "0th": zeroth_ifd,
+            "Exif": exif_ifd,
+            "GPS": gps_ifd,
+            "1st": first_ifd,
+            "thumbnail": thumbnail
+        }
         exif_bytes = piexif.dump(exif_dict)
         im = Image.open("test.jpg")
         im.thumbnail((100, 100), Image.ANTIALIAS)
         im.save("test.jpg", exif=exif_bytes)
-        
+
         im = Image.open("test.jpg")
-        info = im._getexif() 
-        
+        info = im._getexif()
+
         new_exif = self.model.read_exif_data(im)
-    
-        self.assertEqual(new_exif.get('point').coords, (-122.27267499999999, 122.27267499999999))
+
+        self.assertEqual(
+            new_exif.get('point').coords,
+            (-122.27267499999999, 122.27267499999999)
+        )
