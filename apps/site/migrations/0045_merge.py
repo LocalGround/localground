83055ad92767773047ab671c8d720811d121fdbe
Sarah <vanwars@gmail.com>
2018-01-19 11:14:24 -0800

    adding a migration

---
+++ apps/site/migrations/0045_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('site', '0041_auto_20180112_1957'),
+        ('site', '0044_auto_20180111_2020'),
+    ]
+
+    operations = [
+    ]
