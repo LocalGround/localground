f14c0366fd19aed65e0286d6e5d01124f36021c9
Sarah <vanwars@gmail.com>
2018-04-10 17:02:22 -0700

    WIP: working on form validation

--- static/apps/main/views/left/new-map-modal-view.js
+++ static/apps/main/views/left/new-map-modal-view.js
@@ -8,16 +8,31 @@ define ([
         'use strict';
 
         var CreateMapForm = Marionette.ItemView.extend({
+            errors: {},
             initialize: function (opts) {
                 _.extend(this, opts);
                 this.template = Handlebars.compile(CreateMapFormTemplate);
+
+                const datasets = this.app.dataManager.getDatasets();
+                this.formData = {
+                    name: 'Untitled Map',
+                    description: '',
+                    create: '',
+                    existing: 'checked',
+                    datasets: datasets.map(dataset => {
+                        dataset.checked = 'checked';
+                        return dataset;
+                    })
+                };
             },
 
             events: {
                 "click #new-dataset": "toggleCheckboxes",
                 "click #existing-datasets": "toggleCheckboxes"
             },
+
             toggleCheckboxes: function (e) {
+                this.$el.find('#existing-datasets').parent().removeClass('error');
                 var $cb = this.$el.find('.checkbox-list');
                 if (e.target.id === 'new-dataset') {
                     $cb.hide();
@@ -26,34 +41,62 @@ define ([
                 }
             },
             templateHelpers: function () {
-                var helpers = {
-                    name: 'Untitled Map',
-                    datasets: this.app.dataManager.getDatasets()
-                };
-                return helpers;
+                this.formData.errors = this.errors;
+                return this.formData;
             },
 
-            saveMap: function () {
-                this.model.set("name", this.$el.find("#map-name").val());
-                this.model.set("description",
-                    this.$el.find("#map-description").val());
+            applyChanges: function () {
+                this.errors = {};
+                this.formData.name = this.$el.find("#map-name").val();
+                this.formData.description = this.$el.find("#map-description").val();
+                this.formData.create = this.$el.find('#new-dataset').prop('checked') ? "checked" : "";
+                this.formData.existing = this.formData.create ? "" : "checked";
+                console.log(this.formData);
+                //validate name:
+                if (this.formData.name.length > 0) {
+                    this.model.set("name", this.formData.name);
+                } else {
+                    this.errors.name = "A valid map name is required";
+                }
 
-                // Either create a new dataset or select existing one
-                const createNewDataset = this.$el.find('#new-dataset').prop('checked');
-                if (createNewDataset) {
+                // validate dataset selection: either create one or select existing one
+                if (this.formData.create === 'checked') {
                     this.model.set("create_new_dataset", true);
                 } else {
                     const data_sources = []
-                    this.$el.find('input[type="checkbox"]:checked').each(function() {
-                        data_sources.push(this.value);
+                    this.formData.datasets.forEach(dataset => {
+                        if (dataset.dataType === this.value) {
+                            this.checked = '';
+                        }
+                    });
+                    var that = this;
+                    this.$el.find('input[type="checkbox"]').each(function() {
+                        if ($(this).prop('checked')) {
+                            data_sources.push(this.value);
+                            that.formData.datasets.forEach(dataset => {
+                                if (dataset.dataType === this.value) {
+                                    this.checked = 'checked';
+                                }
+                            })
+                        }
                     });
                     if (data_sources.length === 0) {
-                        console.error('no data sources chosen');
-                        return;
+                        this.errors.datasets =  'Please select at least one data source';
+                    } else {
+                        this.model.set("data_sources", JSON.stringify(data_sources));
                     }
-                    this.model.set("data_sources", JSON.stringify(data_sources));
                 }
 
+                // no description validation necessary:
+                this.model.set("description", this.formData.description);
+            },
+
+            saveMap: function () {
+                this.applyChanges()
+                if (!_.isEmpty(this.errors)) {
+                    this.render();
+                    return;
+                }
                 this.model.save(null, {
                     success: this.displayMap.bind(this),
                     failure: this.handleServerError.bind(this)
