e08243e97e2c27b906f2ce8aa17599887af804b3
Riley <20868615+gitriley@users.noreply.github.com>
2018-04-25 14:25:23 -0700

    left panel css finalized

--- static/apps/main/views/left/layer-list-child-view1.js
+++ static/apps/main/views/left/layer-list-child-view1.js
@@ -336,14 +336,14 @@ define(["marionette",
                     this.model.get('metadata').collapsed = false
                     this.$el.find('.symbol').css('height', 'auto');
                     this.$el.find('.symbol-item').css('display', 'block');
-                    this.$el.find('.collapse').removeClass('fa-angle-up');
-                    this.$el.find('.collapse').addClass('fa-angle-down');
+                    this.$el.find('.collapse').removeClass('fa-caret-up');
+                    this.$el.find('.collapse').addClass('fa-caret-down');
                 } else {
                     this.model.get('metadata').collapsed = true;
                     this.$el.find('.symbol').css('height', 0);
                     this.$el.find('.symbol-item').css('display', 'none');
-                    this.$el.find('.collapse').removeClass('fa-angle-down');
-                    this.$el.find('.collapse').addClass('fa-angle-up');
+                    this.$el.find('.collapse').removeClass('fa-caret-down');
+                    this.$el.find('.collapse').addClass('fa-caret-up');
                 }
             },
 
