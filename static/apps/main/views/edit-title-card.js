3d9562da549061eb24e61f1e0744489b6acdb4c1
Riley <20868615+gitriley@users.noreply.github.com>
2018-07-19 13:25:54 -0700

    edit-title-card content now linked to Map model

--- static/apps/main/views/edit-title-card.js
+++ static/apps/main/views/edit-title-card.js
@@ -17,15 +17,48 @@ define([
         className: 'edit-title-card-menu',
 
         templateHelpers: function () {
+            let header, description;
+
+            // once map metadata is fully implemented, these conditional statements can be removed
+            if (this.activeMap.get('metadata')) {
+                if (this.activeMap.get('metadata').titleCardInfo) {
+                    header = this.activeMap.get('metadata').titleCardInfo.header;
+                    description = this.activeMap.get('metadata').titleCardInfo.description;
+                }
+            }
             return {
-                //mapList: this.collection.toJSON(),
-                //map: this.activeMap ? this.activeMap.toJSON() : null
+                header: header,
+                description: description
             };
         },
 
         saveTitleCard: function() {
-            const title = this.$el.find('.title-card_title').val();
-            const description = this.$el.find('.title-card_textarea').val();
+            let header = this.$el.find('.title-card_title').val();
+            let description = this.$el.find('.title-card_textarea').val();
+
+            if (this.activeMap.get('metadata')) {
+                if (this.activeMap.get('metadata').titleCardInfo) {
+
+                    this.activeMap.get('metadata').titleCardInfo.header = header;
+                    this.activeMap.get('metadata').titleCardInfo.description = description;
+
+                    console.log(this.activeMap);
+                    /*
+                    this.activeMap.save(null, {
+                            error: function () {
+                                console.error('error');
+                            },
+                            success: function () {
+                                this.app.vent.trigger('hide-modal)
+                            }
+                        }
+                    );
+                    */
+
+                    // this.app.vent.trigger('hide-modal)
+                }
+            }
+
         }
     });
     return EditTitleCard;
