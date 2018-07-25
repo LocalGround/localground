84360b01ea0b88c4b788decc381aa1275422bba1
Sarah <vanwars@gmail.com>
2018-07-25 15:17:40 -0700

    fixed Grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,24 +14,12 @@ module.exports = function (grunt) {
                     optimize: 'none'
                 }
 	        },
-            style: {
+            main: {
                 options: {
-                    out: './static/build/style.js',
+                    out: './static/build/main.js',
                     baseUrl: "./static/",
                     mainConfigFile: "./static/apps/require-config.js",
-                    name: 'apps/style/kickoff',
-                    removeCombined: 'true',
-                    findNestedDependencies: 'true',
-                    wrapShim: 'true',
-                    optimize: 'none'
-                }
-	        },
-	        dataviewer: {
-                options: {
-                    out: './static/build/dataviewer.js',
-                    baseUrl: "./static/",
-                    mainConfigFile: "./static/apps/require-config.js",
-                    name: 'apps/dataviewer/kickoff',
+                    name: 'apps/main/kickoff',
                     removeCombined: 'true',
                     findNestedDependencies: 'true',
                     wrapShim: 'true',
@@ -61,30 +49,43 @@ module.exports = function (grunt) {
 			},
             //Note: minification doesn't work for ES6. Need a transpiler
             // task
-            /*style: {
-				files: {
-                    './static/build/style.min.js':
-						['./static/build/style.js']
+            // style: {
+			// 	files: {
+            //         './static/build/style.min.js':
+			// 			['./static/build/style.js']
+			// 	}
+			// },
+
+            main: {
+                files: {
+                    './static/build/main.min.js':
+						['./static/build/main.js']
 				}
-			},
-			dataviewer: {
-				files: {
-                    './static/build/dataviewer.min.js':
-						['./static/build/dataviewer.js']
-				}
-			},
+	        },
+			// dataviewer: {
+			// 	files: {
+            //         './static/build/dataviewer.min.js':
+			// 			['./static/build/dataviewer.js']
+			// 	}
+			// },
 			presentation: {
 				files: {
                     './static/build/presentation.min.js':
 						['./static/build/presentation.js']
 				}
-			}*/
+			}
 		}
 
     });
+
     grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['requirejs', 'uglify']);
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-es6-transpiler');
+    grunt.registerTask('default', ['requirejs', 'uglify'])
+
+    //grunt.registerTask('default', ['requirejs', 'es6transpiler', 'uglify']);
+    //grunt.registerTask('default', ['requirejs', 'transpile']);
 };
 
 
