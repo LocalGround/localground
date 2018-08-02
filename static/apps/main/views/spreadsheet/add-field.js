614226dd7cbec9ac984949418afb916fddf4094d
Sarah <vanwars@gmail.com>
2018-08-02 10:12:51 -0700

    WIP: Add field -> choice

--- static/apps/main/views/spreadsheet/add-field.js
+++ static/apps/main/views/spreadsheet/add-field.js
@@ -13,22 +13,75 @@ define ([
          * layer --> current layer (dataset accessible from layer)
          */
         var AddFieldView = Marionette.ItemView.extend({
+            events: {
+                'change #data_type': 'showDetailedOptions',
+                'click .add-new-choice': 'addChoice',
+                'click .remove-choice': 'removeChoice'
+            },
             initialize: function (opts) {
                 _.extend(this, opts);
-                this.model = new Field(
-                    { ordering: this.ordering },
-                    { id: this.dataset.formID }
-                );
+                this.model = new Field({
+                    ordering: this.ordering,
+                    extras: {
+                        choices: [{ name: '' }]
+                    },
+                    data_type: 'text'
+                 }, {
+                     id: this.dataset.formID
+                 });
                 this.template = Handlebars.compile(AddFieldTemplate);
             },
             data_type_error: null,
             col_alias_error: null,
             templateHelpers: function () {
-                var helpers = {
+                return {
                     data_type_error: this.data_type_error,
-                    col_alias_error: this.col_alias_error
+                    col_alias_error: this.col_alias_error,
+                    extras: this.model.get('extras'),
+                    dataType: this.$el.find('#data_type').val()
                 };
-                return helpers;
+            },
+            showDetailedOptions: function () {
+                const dataType = this.$el.find('#data_type').val();
+                this.$el.find('.numbers, .choices').hide();
+                switch (dataType) {
+                    case 'number':
+                        this.$el.find('.row.numbers').show();
+                        break;
+                    case 'choice':
+                        this.$el.find('.row.choices').show();
+                        break;
+                }
+            },
+            commitData: function () {
+                // Note: doesn't save, but applies the data to the model:
+                //save name:
+                this.model.set('col_alias', this.$el.find('#col_alias').val());
+
+                //save choices:
+                const choices = [];
+                this.$el.find('.choice').each(function () {
+                    choices.push({
+                        'name': $(this).val()
+                    });
+                });
+                this.model.get('extras').choices = choices;
+            },
+            addChoice: function (e) {
+                if (e) { e.preventDefault(); }
+                this.commitData();
+                this.model.get('extras').choices.push({'name': ''});
+                this.render();
+
+            },
+            removeChoice: function (e) {
+                if (e) { e.preventDefault(); }
+                $(e.target).parent().parent().remove();
+                this.commitData();
+                this.render();
+            },
+            onRender: function () {
+                this.showDetailedOptions();
             },
             _clearErrorMessages: function () {
                 this.data_type_error = null;
