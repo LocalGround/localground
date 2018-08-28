d9672cda04ac936ac10b29f51149c4154f751ecd
Riley <20868615+gitriley@users.noreply.github.com>
2018-08-28 15:25:00 -0700

    updated edit-media-info tests

--- static/tests/spec/lib/edit-media-info-test.js
+++ static/tests/spec/lib/edit-media-info-test.js
@@ -5,7 +5,7 @@ define([
 ],
     function (Backbone, EditMediaInfo) {
         'use strict';
-        const initEditMediaInfo = function (scope) {
+        const initEditMediaInfo = function (scope, mediaType) {
 
             // 1) add spies for all relevant objects:
             spyOn(EditMediaInfo.prototype, 'initialize').and.callThrough();
@@ -17,17 +17,27 @@ define([
             scope.fixture = setFixtures('<div></div>');
 
             // 3) initialize view:
-            const photo = scope.app.dataManager.getCollection('photos').get(20);
+            let mediaModel;
+            if (mediaType === 'photos') {
+                mediaModel = scope.app.dataManager.getCollection('photos').get(20);
+            } else if (mediaType === 'videos') {
+                mediaModel = scope.app.dataManager.getCollection('videos').get(50);
+            } else if (mediaType === 'audio') {
+                mediaModel = scope.app.dataManager.getCollection('audio').get(3);
+            } else {
+                mediaModel = scope.app.dataManager.getCollection('photos').get(20);
+            }
+             
             scope.editMediaInfo = new EditMediaInfo({
                 app: scope.app,
-                model: photo
+                model: mediaModel
             });
             scope.editMediaInfo.render();
         };
 
-        describe('EditMediaInfo', function () {
+        describe('EditMediaInfo (with photo)', function () {
             beforeEach(function () {
-                initEditMediaInfo(this);
+                initEditMediaInfo(this, 'photo');
             });
 
             it('initialize works', function() {
@@ -44,6 +54,15 @@ define([
                 expect(this.editMediaInfo.$el.find('#media-caption').val()).toEqual('');
                 expect(this.editMediaInfo.$el.find('#media-name').val()).toEqual('DSC04705.JPG');
             });
+
+            it('renders photos properly', function() {
+                expect(this.editMediaInfo.$el).toContainElement('img');
+                expect(this.editMediaInfo.$el).not.toContainElement('iframe');
+                expect(this.editMediaInfo.$el).not.toContainElement('.edit-audio');
+                expect(this.editMediaInfo.$el.find('img')[0].src).toEqual(
+                    this.editMediaInfo.model.get('path_large')
+                );
+            });
             
             it('saveMediaInfo() works', function() {
                 this.editMediaInfo.$el.find('#media-attribution').val('George Washington');
@@ -59,6 +78,65 @@ define([
                 expect(this.editMediaInfo.model.get('attribution')).toEqual('George Washington');
                 expect(this.editMediaInfo.model.get('caption')).toEqual('photo of building');
                 expect(this.editMediaInfo.model.get('name')).toEqual('building');
+            });
+        });
+        describe('EditMediaInfo (with video)', function () {
+            beforeEach(function () {
+                initEditMediaInfo(this, 'videos');
+            });
+
+            it('initialize works', function() {
+                expect(this.editMediaInfo).toEqual(jasmine.any(EditMediaInfo));
+                expect(this.editMediaInfo.model.id).toEqual(50);
+            });
+
+            it('renders correct info', function() {
+                expect(this.editMediaInfo.model.get('attribution')).toEqual(null);
+                expect(this.editMediaInfo.model.get('caption')).toEqual(null);
+                expect(this.editMediaInfo.model.get('name')).toEqual('Untitled');
+
+                expect(this.editMediaInfo.$el.find('#media-attribution').val()).toEqual('');
+                expect(this.editMediaInfo.$el.find('#media-caption').val()).toEqual('');
+                expect(this.editMediaInfo.$el.find('#media-name').val()).toEqual('Untitled');
+            });
+
+            it('renders photos properly', function() {
+                expect(this.editMediaInfo.$el).toContainElement('iframe');
+                expect(this.editMediaInfo.$el).not.toContainElement('img');
+                expect(this.editMediaInfo.$el).not.toContainElement('.edit-audio');
+                expect(this.editMediaInfo.$el.find('iframe')[0].src).toEqual(
+                    `https://www.youtube.com/embed/${this.editMediaInfo.model.get('video_id')}?ecver=1`
+                );
+            });
+        });
+        describe('EditMediaInfo (with audio)', function () {
+            beforeEach(function () {
+                initEditMediaInfo(this, 'audio');
+            });
+
+            it('initialize works', function() {
+                expect(this.editMediaInfo).toEqual(jasmine.any(EditMediaInfo));
+                expect(this.editMediaInfo.model.id).toEqual(3);
+            });
+
+            it('renders correct info', function() {
+                expect(this.editMediaInfo.model.get('attribution')).toEqual('vanwars');
+                expect(this.editMediaInfo.model.get('caption')).toEqual(null);
+                expect(this.editMediaInfo.model.get('name')).toEqual('tmpvpqx0b.mp3');
+
+                expect(this.editMediaInfo.$el.find('#media-attribution').val()).toEqual('vanwars');
+                expect(this.editMediaInfo.$el.find('#media-caption').val()).toEqual('');
+                expect(this.editMediaInfo.$el.find('#media-name').val()).toEqual('tmpvpqx0b.mp3');
+            });
+
+            it('renders photos properly', function() {
+                expect(this.editMediaInfo.$el).toContainElement('.edit-audio');
+                expect(this.editMediaInfo.$el).not.toContainElement('iframe');
+                expect(this.editMediaInfo.$el).not.toContainElement('img');
+                
+                expect(this.editMediaInfo.$el.find('source')[0].src).toEqual(
+                    this.editMediaInfo.model.get('file_path')
+                );
             });
         });
     });
