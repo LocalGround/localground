90067970d1754026532af2d4743b5a870facd136
Riley <20868615+gitriley@users.noreply.github.com>
2018-09-25 19:44:24 -0700

    implemented tabs for switching between map list and dataset list

--- static/apps/project_detail/views/project-info-container.js
+++ static/apps/project_detail/views/project-info-container.js
@@ -11,19 +11,53 @@ define(["marionette",
                 mapListRegion: '.project_map-list',
                 datasetListRegion: '.project_dataset-list'
             },
-            // events: {
-            //     'click .title': 'openRenameForm'
-            // },
+            events: {
+                'click #maps-section_button': 'showMaps',
+                'click #datasets-section_button': 'showDatasets',
+            },
             initialize: function (opts) {
                 _.extend(this, opts);
                 this.template = Handlebars.compile(InfoContainerTemplate);
-                
+                this.displayMode = 'maps';
             },
 
             onRender: function () {
+                
                 this.showMapList();
                 this.showDatasetList();
+                this.toggleDisplayMode(this.displayMode);
             },
+
+            toggleDisplayMode: function(displayMode) {
+                if (displayMode === 'maps') {
+                    this.datasetListRegion.$el.hide();
+                    this.mapListRegion.$el.show();
+                    this.$el.find('#maps-section_button').addClass('active').removeClass('inactive');
+                    this.$el.find('#datasets-section_button').addClass('inactive').removeClass('active');
+                } else if (displayMode === 'datasets') {
+                    this.mapListRegion.$el.hide();
+                    this.datasetListRegion.$el.show();
+                    this.$el.find('#datasets-section_button').addClass('active').removeClass('inactive');
+                    this.$el.find('#maps-section_button').addClass('inactive').removeClass('active');
+                    
+                }
+            },
+
+            showMaps: function() {
+                if (this.displayMode !== 'maps') {
+                    this.displayMode = 'maps';
+                    this.toggleDisplayMode(this.displayMode);
+                }
+                
+            },
+
+            showDatasets: function() {
+                if (this.displayMode !== 'datasets') {
+                    this.displayMode = 'datasets';
+                    this.toggleDisplayMode(this.displayMode);
+                }
+            },
+
             showMapList: function() {
                 this.mapListView = new MapListView({
                     model: this.model,
