d38ace6b0adb8b1abbd8325b3f8045ca9f6e3f2a
Riley <20868615+gitriley@users.noreply.github.com>
2018-10-07 11:33:03 -0700

    refactored Map collection comparators

--- static/collections/maps.js
+++ static/collections/maps.js
@@ -6,31 +6,25 @@ function (Map, BasePageableWithProject) {
         name: 'Maps',
         key: 'maps',
         url: '/api/0/maps/',
-        // comparator: function (a) {
-        //     console.log('sorting');
-        //     return a.get('name');
-        // },
+        
         initialize: function() {
            this.setComparator('name') 
         },
         setComparator: function(sortBy) {
-            switch(sortBy) {
-                case 'name':
-                    this.comparator = this.sortByName;
-                    break;
-                case 'created_by':
-                    this.comparator = this.sortByCreator;
-                    break;
-                case 'date_modified':
-                    this.comparator = this.sortByDateModified;
-                    break;
-                case 'access_level':
-                    this.comparator = this.sortByAccessLevel;
-                    break;
-                default:
-                    this.comparator = this.compareByName;
+            
+            this.comparator = this.comparatorOptions()[sortBy];
+        },
+
+
+        comparatorOptions: function() {
+            return {
+                name: this.sortByName,
+                created_by: this.sortByCreator,
+                date_modified: this.sortByDateModified,
+                access_level: this.sortByAccessLevel
             }
         },
+
         sortByName: function (a) {
             return a.get('name');
         },
@@ -38,6 +32,7 @@ function (Map, BasePageableWithProject) {
             return a.get('owner');
         },
         sortByDateModified: function(mapA, mapB) {
+            // sort newest to oldest
             if (mapA.get('time_stamp') > mapB.get('time_stamp')) return -1; // before
             if (mapB.get('time_stamp') > mapA.get('time_stamp')) return 1; // after
             return 0; // equal
