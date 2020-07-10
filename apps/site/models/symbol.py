0ecb79bb9b5b22df842fa8151507b42c7d49580f
Sarah <vanwars@gmail.com>
2020-07-10 13:16:16 -0500

    fixing vector symbols

--- apps/site/models/symbol.py
+++ apps/site/models/symbol.py
@@ -408,44 +408,58 @@ class Symbol(object):
         return opts
 
     def check_if_match(self, recordModel):
-        # import random
-        # # flip a coin:
-        # return random.choice([True, False])
         rule = RuleParser(self.get_rule(None))
         return rule.check_model(recordModel)
     
     def generate_svg(self):
-        iconSize = [self.width, self.height]
-        iconAnchor = [self.width / 2.0, self.height / 2.0]
-        popupAnchor = [0, 0]
+        '''
+        Notes:
+         *  viewBox: min-x, min-y, width, height
+         *  I've adjusted all of the viewBoxes to have 10px paddings
+            to allow for up to a 10px border. This is factored into the 
+            width / height calculation
+
+        This code is a little convoluted, and I'm sure could be simplified
+        if we had SVG paths of the same sizes.
+        '''
+        border_padding = 10
         if self.shape == 'circle':
-            self.strokeWeight *= 0.5
             path = 'M14,7.5c0,3.5899-2.9101,6.5-6.5,6.5S1,11.0899,1,7.5S3.9101,1,7.5,1S14,3.9101,14,7.5z'
-            bbox = '0.5 0.5 14 14'
+            x1, y1, w, h = -1.5, -1.5, 18, 18
         elif self.shape == 'square':
-            self.strokeWeight *= 0.5
             path = 'M13,14H2c-0.5523,0-1-0.4477-1-1V2c0-0.5523,0.4477-1,1-1h11c0.5523,0,1,0.4477,1,1v11C14,13.5523,13.5523,14,13,14z'
-            bbox = '0.5 0.5 14 14'
+            x1, y1, w, h = -1.5, -1.5, 18, 18
         elif self.shape == 'plus':
-            self.strokeWeight *= 25
-            bbox = '-130 -130 1200 1200'
+            x1, y1, w, h = -250, -250, 1550, 1550
             path = 'M992 384h-352v-352c0-17.672-14.328-32-32-32h-192c-17.672 0-32 14.328-32 32v352h-352c-17.672 0-32 14.328-32 32v192c0 17.672 14.328 32 32 32h352v352c0 17.672 14.328 32 32 32h192c17.672 0 32-14.328 32-32v-352h352c17.672 0 32-14.328 32-32v-192c0-17.672-14.328-32-32-32z'
         elif self.shape == 'cross':
-            self.strokeWeight *= 25
-            bbox = '-130 -130 1200 1200'
+            x1, y1, w, h = -250, -250, 1550, 1550
             path = 'M 1014.66 822.66 c -0.004 -0.004 -0.008 -0.008 -0.012 -0.01 l -310.644 -310.65 l 310.644 -310.65 c 0.004 -0.004 0.008 -0.006 0.012 -0.01 c 3.344 -3.346 5.762 -7.254 7.312 -11.416 c 4.246 -11.376 1.824 -24.682 -7.324 -33.83 l -146.746 -146.746 c -9.148 -9.146 -22.45 -11.566 -33.828 -7.32 c -4.16 1.55 -8.07 3.968 -11.418 7.31 c 0 0.004 -0.004 0.006 -0.008 0.01 l -310.648 310.652 l -310.648 -310.65 c -0.004 -0.004 -0.006 -0.006 -0.01 -0.01 c -3.346 -3.342 -7.254 -5.76 -11.414 -7.31 c -11.38 -4.248 -24.682 -1.826 -33.83 7.32 l -146.748 146.748 c -9.148 9.148 -11.568 22.452 -7.322 33.828 c 1.552 4.16 3.97 8.072 7.312 11.416 c 0.004 0.002 0.006 0.006 0.01 0.01 l 310.65 310.648 l -310.65 310.652 c -0.002 0.004 -0.006 0.006 -0.008 0.01 c -3.342 3.346 -5.76 7.254 -7.314 11.414 c -4.248 11.376 -1.826 24.682 7.322 33.83 l 146.748 146.746 c 9.15 9.148 22.452 11.568 33.83 7.322 c 4.16 -1.552 8.07 -3.97 11.416 -7.312 c 0.002 -0.004 0.006 -0.006 0.01 -0.01 l 310.648 -310.65 l 310.648 310.65 c 0.004 0.002 0.008 0.006 0.012 0.008 c 3.348 3.344 7.254 5.762 11.414 7.314 c 11.378 4.246 24.684 1.826 33.828 -7.322 l 146.746 -146.748 c 9.148 -9.148 11.57 -22.454 7.324 -33.83 c -1.552 -4.16 -3.97 -8.068 -7.314 -11.414 Z'
         else:
             # assume shape is marker:
-            self.height = self.width * 1.2
+            x1, y1, w, h = -1, -1, 17.5, 17.5
+            path = 'M7.5,0C5.0676,0,2.2297,1.4865,2.2297,5.2703 C2.2297,7.8378,6.2838,13.5135,7.5,15c1.0811-1.4865,5.2703-7.027,5.2703-9.7297C12.7703,1.4865,9.9324,0,7.5,0z'
+
+        if self.shape in ['circle', 'square', 'plus', 'cross']:
+            self.width += border_padding
+            self.height += border_padding
+            iconSize = [self.width, self.height]
+            iconAnchor = [self.width / 2.0, self.height / 2.0]
+            popupAnchor = [0, 0]
+        else:
+            self.height = self.width * 1.35
+            self.width += border_padding
+            self.height += border_padding
             iconSize = [self.width, self.height]
             iconAnchor = [self.width / 2.0, self.height]
             popupAnchor = [0, -self.height * 0.9]
-            self.strokeWeight *= 0.5
-            path = 'M7.5,0C5.0676,0,2.2297,1.4865,2.2297,5.2703 C2.2297,7.8378,6.2838,13.5135,7.5,15c1.0811-1.4865,5.2703-7.027,5.2703-9.7297C12.7703,1.4865,9.9324,0,7.5,0z'
-            bbox = '1.5 1 12.5 12.5'
+
+    
+        bbox = '{0} {1} {2} {3}'.format(x1, y1, w, h)
+        # print(bbox)   
         
         svg = '<svg width="{width}" height="{height}" viewBox="{bbox}" xmlns="http://www.w3.org/2000/svg">'
-        svg += '<path stroke="{strokeColor}" stroke-width="{strokeWeight}" fill-opacity="{fillOpacity}" stroke-opacity="{strokeOpacity}" fill="{fillColor}" d="{path}" />'
+        svg += '<path vector-effect="non-scaling-stroke" stroke="{strokeColor}" stroke-width="{strokeWeight}" fill-opacity="{fillOpacity}" stroke-opacity="{strokeOpacity}" fill="{fillColor}" d="{path}" />'
         svg += '</svg>'
         svg = svg.format(
             width=self.width,
