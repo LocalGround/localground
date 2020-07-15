f047ad264f72469cb58e256aa39f7f132417223b
Sarah <vanwars@gmail.com>
2020-07-15 10:00:05 -0500

    adding foldable code

--- static/js/code-view.js
+++ static/js/code-view.js
@@ -2,15 +2,20 @@ class CodeView {
     constructor (rawJSON) {
         Object.assign(this, mixins);
         this.rawJSON = rawJSON;
+        this.jsonViewer = new JSONViewer();
         this.render();
     }
 
     render () {
         document.querySelector('header h1').innerHTML = this.rawJSON.name;
         document.querySelector('main').className = '';
-        document.querySelector('main').innerHTML = '<pre></pre>'; 
+        document.querySelector('main').innerHTML = '';
+        // document.querySelector('main').innerHTML = '<pre></pre>'; 
         document.querySelector('body').style.overflowY = 'scroll'; 
-        document.querySelector('pre').innerHTML = `${JSON.stringify(this.rawJSON, null, 3)}`;
+        
+        document.querySelector("main").appendChild(this.jsonViewer.getContainer());
+        // jsonViewer.showJSON(json, maxLvl, colAt);
+        this.jsonViewer.showJSON(this.rawJSON, -1, 2);
     }
 
     destroy () {
