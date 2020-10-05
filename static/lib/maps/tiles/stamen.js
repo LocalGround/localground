e4a0b35ce516bb0953a859f74c672e72894eccc2
Sarah <vanwars@gmail.com>
2020-10-05 16:27:50 -0500

    Update stamen.js

--- static/lib/maps/tiles/stamen.js
+++ static/lib/maps/tiles/stamen.js
@@ -2,17 +2,17 @@ define(["jquery"], function ($) {
     "use strict";
     var Stamen = function (opts) {
         /*
-         * http://a.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg
-         * http://maps.stamen.com/#terrain/12/37.7706/-122.3782
+         * https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png
+         * https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/12/37.7706/-122.3782
          */
         this.maxZoom = opts.max;
         this.name = opts.name;
         this.ext = opts.url.split('{y}');
         this.ext = (this.ext.length > 0) ? this.ext[1] : '';
-        this.url = opts.url.split('{z}')[0].split('//')[1];
+        this.url = opts.url.split('{s}')[1];
         this.tileSize = new google.maps.Size(256, 256);
         this.getTile = function (coord, zoom) {
-            var url = 'http://' + ['', 'a.', 'b.'][parseInt(Math.random() * 3, 10)] + this.url;
+            var url = 'https://stamen-tiles-' + 'abcd'[Math.floor(Math.random() * 4)] + this.url;
             return $('<div></div>').css({
                 'width': '256px',
                 'height': '256px',
